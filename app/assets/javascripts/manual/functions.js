bbf0465ba6a75806514b2bba6b9de22b7b761060
diegoeis <diegoeis@gmail.com>
2012-04-10 15:25:42 -0300

    partial new layout

--- app/assets/javascripts/manual/functions.js
+++ app/assets/javascripts/manual/functions.js
@@ -1,4 +1,29 @@
+
+
+
+var resizeBoxes = function(){
+  var contentHeight = $('.main').outerHeight();
+  var sidebarHeight = $('aside').height();
+
+  if (contentHeight > sidebarHeight) {
+    $('aside').css("min-height", contentHeight);
+  } else {
+    $('aside').css("height", "auto");
+  }
+
+  if (sidebarHeight > contentHeight) {
+    $('.main').css("min-height", sidebarHeight - 20);
+  } else {
+    $('.main').css("height", "auto");
+  }
+  // var mainDivHeight;
+  // mainDivHeight = $(".main").height();
+  // $("aside.boxes").css("height", mainDivHeight+60);
+};
+
+$('.main').on('shown', function(){resizeBoxes()});
+
 $(document).ready(function(){
   SyntaxHighlighter.all()
-  $('.guideAreas a[href="' + window.location.pathname + '"]').parent().addClass('selected');
+  resizeBoxes()
 });
