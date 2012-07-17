1d50db6283fad862100de382a327c6c9e1d200e6
Diego Eis <diego@tableless.com.br>
2012-07-17 12:40:34 -0300

    arrumando mediaqueries do IE.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery-ui-1.8.18.custom.min.js
 //= require modernizr
-//= require css3-mediaqueries
 //= require chosen.jquery.min
 //= require bootstrap/js/bootstrap
 //= require scripts
