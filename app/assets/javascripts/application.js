f8835b0134ff13f9af473e7356486ca1f48a91a8
Renato Nitta <renatocn@gmail.com>
2013-01-30 16:52:14 -0200

    removendo arquivos desatualizados do manifest

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,18 +4,14 @@
 //
 // < VENDOR
 //= require jquery
-//= require analytics_trackevent
 //= require modernizr
 //= require bootstrap/js/bootstrap
 //= require select2.min
 //= require jquery-ui.min
 //= require masked-input
-//= require passwordStrength
 //= require cookie
 // VENDOR >
 //
 // < APP
 //= require locastyle
-//= require accessibility
-//= require browser-detect
 // APP >
