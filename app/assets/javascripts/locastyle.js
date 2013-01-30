b7c3ae273bb1e98c696f76bdc90d47d962e64694
Renato Nitta <renatocn@gmail.com>
2013-01-30 16:55:16 -0200

    primeira implementação refatorada e testada, toggle text

--- app/assets/javascripts/locastyle.js
+++ app/assets/javascripts/locastyle.js
@@ -1,6 +1,21 @@
 Locastyle = (function() {
-  Locastyle.prototype = {
-    init: function () {
+  Locastyle.prototype.methods = {
+    init: function (dom_scope) {
+    },
+
+    toggleTextOnClick: function(dom_scope) {
+      var self = this;
+      $('[data-toggle_text="click"]', dom_scope).on("click", function(e) {
+        e.preventDefault();
+        self.toggleText(this);
+      });
+    },
+
+    toggleText: function(element) {
+      var text, replacementText;
+      text = $(element).html();
+      replacementText = $(element).data("text");
+      $(element).text(replacementText).data("text", text);
     }
-  };
+  }
 });
--- spec/javascripts/locastyle/locastyle_spec.js
+++ spec/javascripts/locastyle/locastyle_spec.js
@@ -15,7 +15,33 @@ describe("Locastyle", function() {
 
       it("should have an inherited init function", function () {
         var locastyle = new Locastyle();
-        expect(typeof(locastyle.init)).toEqual("function");
+        expect(typeof(locastyle.methods.init)).toEqual("function");
+      });
+    });
+  });
+
+  describe("Toggle text", function () {
+    beforeEach(function () {
+      loadFixtures('locastyle.html');
+    });
+
+    describe("when an element has data-toggle_text='click' attribute", function () {
+      it("should replace the text when clicked on it", function () {
+        var dom_scope = $("#locastyle_fixture");
+        var locastyle = new Locastyle();
+        var textToBeApplied = $("#toggle_text").data("text");
+        locastyle.methods.toggleTextOnClick(dom_scope);
+        $('[data-toggle_text="click"]').click();
+        expect($("#toggle_text").text()).toEqual(textToBeApplied);
+      });
+
+      it("should also replace the data-text attribute when clicked on it", function () {
+        var dom_scope = $("#locastyle_fixture");
+        var locastyle = new Locastyle();
+        var text = $("#toggle_text").text();
+        locastyle.methods.toggleTextOnClick(dom_scope);
+        $('[data-toggle_text="click"]').click();
+        expect($("#toggle_text").data("text")).toEqual(text);
       });
     });
   });
