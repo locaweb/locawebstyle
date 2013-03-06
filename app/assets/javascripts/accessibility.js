eb6ff0f552d4a7d2ff1a553e29171cf9ce38ec61
Diego Eis <diegoeis@gmail.com>
2013-03-06 14:55:24 -0300

    renaming headerPrincipal class to #header

--- app/assets/javascripts/accessibility.js
+++ app/assets/javascripts/accessibility.js
@@ -32,8 +32,8 @@ $(function(){
 	$('a.btn, .modal-header .close').attr('role','button');
 	$('.boxGray').attr('role','region');
 	$('.boxGray h2').attr('role','presentation');
-	$('.headerPrincipal').attr('role','banner');
-	$('.content').attr('role','main');
+	$('.header').attr('role','banner');
+	$('.content').attr('role','main'); // criar essa classe nos produtos ??
 	$('.sidebar').attr('role','complementary');
 	$('input.required').attr('aria-required','true');
 	$('.lnkCoverAll').attr('tabindex','3');
