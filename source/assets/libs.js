ec812e9e8c4c83ff403dad4b241da3fe84395d24
Renato Nitta <renatocn@gmail.com>
2013-07-23 19:37:17 -0300

    Separando as libs externas e escolhendo a ordem de carregamento de cada pacote de asset

---
+++ source/assets/libs.js
@@ -0,0 +1,7 @@
+//= require locastyle/javascripts/vendor/jquery
+//= require locastyle/javascripts/vendor/modernizr
+//= require locastyle/javascripts/vendor/select2.min
+//= require locastyle/javascripts/vendor/jquery-ui.min
+//= require locastyle/javascripts/vendor/masked-input
+//= require locastyle/javascripts/vendor/cookie
+//= require locastyle/javascripts/vendor/validate
