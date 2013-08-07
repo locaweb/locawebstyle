a1062928798ee83e58852c0c187c58be99ddd1ac
Renato Nitta <renatocn@gmail.com>
2013-08-07 18:35:03 -0300

    minificando js e css no build

--- config.rb
+++ config.rb
@@ -67,10 +67,10 @@ page "/manual/exemplos/*", :layout => "system-example"
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  #activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  #activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :cache_buster
