b6438f06a62555fac9d4ad2df29ee165bf0df36b
Renato Nitta <renatocn@gmail.com>
2013-08-07 18:29:02 -0300

    configurando path diferente para assets no build

--- config.rb
+++ config.rb
@@ -85,4 +85,7 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
+  set :css_dir, 'locawebstyle/assets'
+  set :js_dir, 'locawebstyle/assets'
+  set :images_dir, 'locawebstyle/assets/manual/img'
 end
