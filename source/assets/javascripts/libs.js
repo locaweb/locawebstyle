c86a8f738bb0617728a8611daf0e8d8f1270000d
Diego Eis <diegoeis@gmail.com>
2013-08-12 16:16:31 -0300

    Apagando o Locastyle para iniciar segunda versão.
    Será uma versão totalmente iniciada do zero. Fresh new.

--- source/assets/javascripts/libs.js
+++
@@ -1,6 +0,0 @@
-//= require libs/_jquery
-//= require libs/_modernizr
-//= require libs/_select2.min
-//= require libs/_jquery-ui.min
-//= require libs/_masked-input
-//= require libs/_cookie
--- source/assets/javascripts/locastyle/_initializer.js
+++
@@ -1,5 +0,0 @@
-$(window).load(function() {
-  window.locastyle = new Locastyle();
-
-  locastyle.base.init($(document));
-});
