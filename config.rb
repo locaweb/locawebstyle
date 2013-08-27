d15dac0ce4268f1b567a5c7a18a3f9afeccb9ca8
Diego Eis <diegoeis@gmail.com>
2013-08-27 10:43:12 -0300

    mudando versao estavel na documentacao do locastyle.

--- config.rb
+++ config.rb
@@ -52,7 +52,7 @@
 ####
 set :only_assets, "http://developer.locaweb.com.br/assets/edge.zip"
 set :download_github, "https://github.com/locaweb/locawebstyle/archive/master.zip"
-set :stable, "1.1.19"
+set :stable, "1.1.9"
 
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
