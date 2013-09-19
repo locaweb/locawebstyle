e149fde72048c2577796817db093fb5497998412
Diego Eis <diegoeis@gmail.com>
2013-09-19 15:54:35 -0300

    Touchstart
    Inserindo evento touchstart para tirar os milisegundos irritantes dos
    browsers mobiles.

--- source/assets/javascripts/locastyle/_mobile_functions.js
+++ source/assets/javascripts/locastyle/_mobile_functions.js
@@ -10,21 +10,23 @@ locastyle.mobile = (function() {
   }
 
   function mobileLeftBar() {
-    $('.control-menu').on('click', function(){
+    $('.control-menu').on('click touchstart', function(e){
       $('html').removeClass('right-bar').toggleClass('left-bar');
+      e.preventDefault();
     });
   }
 
   function mobileRightBar() {
-    $('.control-sidebar').on('click', function(){
+    $('.control-sidebar').on('click touchstart', function(e){
       $('html').toggleClass('right-bar').removeClass('left-bar');
+      e.preventDefault();
     });
   }
 
   function mobileBarOverlay() {
     $('body').append('<span class="overlay-bar"></span>');
 
-    $('.overlay-bar').on('click', function(){
+    $('.overlay-bar').on('click touchstart', function(){
       $('html').removeClass('right-bar').removeClass('left-bar');
     });
 
