eba0958efac4653a77bfdcbec9752d3bd667da6a
Deivid Marques <deivid.goncalves@locaweb.com.br>
2013-10-11 15:21:52 -0300

    inserindo navegacao guiada

---
+++ source/assets/javascripts/locastyle/_guided_tour.js
@@ -0,0 +1,62 @@
+Locastyle.prototype.guidedTour = (function() {
+	'use strict';
+
+	function init(jsonSteps){
+		checkTour(jsonSteps);
+		keyCode();
+	}
+
+	var jsonTour;
+
+	function setTour(tour){
+		jsonTour = tour;
+		$('.lnk-suggestions').on({click: openWelcomeTour});
+		$('.guided-tour .btn-tour').on({click: initTour});
+		$('.guided-tour .btn-close').on({click: closeWelcomeTour});
+	}
+
+	function openWelcomeTour(){
+		$('.guided-tour').addClass('on');
+		return false;
+	}
+
+	function closeWelcomeTour(){
+		$('.guided-tour').removeClass('on');
+	}
+
+	function initTour(){
+		hopscotch.startTour(jsonTour);
+		keyCode();
+		closeWelcomeTour();
+		return false;
+	}
+
+	function keyCode(element){
+		var left = 39;
+		var right = 37;
+		var esc = 27;
+		document.onkeyup = function(e){
+			var key = event.keyCode
+			if(key == left ){	hopscotch.nextStep(); }
+			if(key == right){	hopscotch.prevStep(); }
+			if(key == esc  ){	hopscotch.endTour();  }
+		}
+	}
+
+	function userAborted(xhr) {
+		return !xhr.getAllResponseHeaders();
+	}
+
+	function checkTour(jsonSteps){
+		if(jsonSteps && hopscotch){
+			setTour(jsonSteps);
+		}
+	}
+
+	return {
+		init: init,
+		openWelcomeTour: openWelcomeTour,
+		closeWelcomeTour: closeWelcomeTour
+	};
+
+}());
