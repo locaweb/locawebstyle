f9a384c6a4c15d70fc65728d660c8ffdad3357dd
Wylkon Cardoso <wylkon@locas-Mac-mini.local>
2013-10-14 13:30:15 -0300

    Alterado o nome da classe: .sliderMobile para .slider-mobile

--- source/assets/javascripts/locastyle/_mobile_functions.js
+++ source/assets/javascripts/locastyle/_mobile_functions.js
@@ -48,7 +48,7 @@ locastyle.mobile = (function() {
 
 
   function sliderMobile() {
-    var si = $('.media-mobile .sliderMobile').royalSlider({
+    var si = $('.media-mobile .slider-mobile').royalSlider({
       addActiveClass: true,
       arrowsNav: false,
       startSlideId: 1,
