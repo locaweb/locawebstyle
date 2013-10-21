223d28c32a00f0c8139ef0b8da1a39b325104c61
Diego Eis <diegoeis@gmail.com>
2013-10-21 17:52:12 -0200

    Criando uma função de checkboxes nas tabelas.
    Fiz pareando com o @letanure. Ele fez uma parte e eu fiz outra.

--- source/assets/javascripts/locastyle/_base.js
+++ source/assets/javascripts/locastyle/_base.js
@@ -97,26 +97,38 @@ var locastyle = (function() {
 
   // Quando as tabelas tiverem checkboxes e mais de dois checkboxes forem marcados, será exibido um box com ações (ex: excluir, enviar, duplicar e etc).
   function showActions () {
-    var tableCheckboxes = $('.table-style input[type="checkbox"]');
+    $('.table-style').each(function() {
 
-    $(tableCheckboxes).change( function () {
-      var tableCheckboxesChecked = $('.table-style input[type="checkbox"]:checked').length
+      var $tableStyle = $(this);
+      var $wellTableId = $(this).prop('id');
+      var $checkAll   = $tableStyle.find('th input[type="checkbox"]');
+      var $checkboxes = $tableStyle.find('td input[type="checkbox"]');
 
-      if( tableCheckboxesChecked >=2 ) {
+      // Quando clica no checkbox principal, seleciona todos os outros
+      $checkAll.on('change', function(){
+        $checkboxes.prop('checked', $checkAll.prop('checked') )
+        showWellTable();
+      });
 
-        $(this).parents('.table-style').parent().find('.well strong').text(tableCheckboxesChecked);
-        $(this).parents('.table-style').parent().find('.well').removeClass('hidden')
+      // Quando seleciona todos os checkboxes, seleciona o checkbox principal também
+      $checkboxes.on('change', function () {
+        $checkAll.prop('checked', $tableStyle.find('td input[type="checkbox"]:checked').size() == $checkboxes.size() );
+        showWellTable();
+      });
 
-      } else {
+      function showWellTable() {
+        // Verifica quantidade de itens checados para mostrar as opções de ação
+        if ( $tableStyle.find('td input[type="checkbox"]:checked').size() > 1 ) {
+          $('[data-target="'+ $wellTableId +'"]').removeClass('hidden');
+        } else {
+          $('[data-target="'+ $wellTableId +'"]').addClass('hidden');
+        }
 
-        $(this).parents('.table-style').parent().find('.well').addClass('hidden')
+        // Conta quantos checkboxes existe checados e marca no Counter
+        $('[data-target="'+ $wellTableId +'"]').find('.counterChecks').html($tableStyle.find('td input[type="checkbox"]:checked').size());
       }
-    });
 
-    // Em ambiente mobile os botões exibidos no WELL serão modificados com a class "btn-sm"
-    if(locastyle.breakpoint == "media-mobile") {
-      $('.well .btn').addClass('btn-xs');
-    }
+    });
 
   }
 
