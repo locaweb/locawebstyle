8daf964a0f728345b19e9c16253b24e5abf7f288
Diego Eis <diegoeis@gmail.com>
2013-11-13 18:04:19 -0200

    Arrumando versão estável no manual novo.

--- config.rb
+++ config.rb
@@ -73,7 +73,7 @@ end
 ####
 set :only_assets, "http://developer.locaweb.com.br/assets/edge.zip"
 set :download_github, "https://github.com/locaweb/locawebstyle/archive/master.zip"
-set :stable, "1.1.19"
+set :stable, "2.0.0"
 
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
