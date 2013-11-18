08399ed5636b107ca3d9994f83efe2e8612ed834
letanure <letanure@gmail.com>
2013-11-18 19:33:36 -0200

    diretorio de fontes no config

--- config.rb
+++ config.rb
@@ -81,6 +81,7 @@ set :stable, "2.0.0-a4"
 set :css_dir, 'assets/stylesheets'
 set :js_dir, 'assets/javascripts'
 set :images_dir, 'assets/images'
+set :fonts_dir, 'assets/fonts'
 
 set :bootstrap_js, '/assets/bootstrap/js/bootstrap.js'
 set :bootstrap_css, '/assets/bootstrap/css/bootstrap.css'
