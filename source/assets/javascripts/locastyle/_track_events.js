492bac177c27d38450e5cb06d8495a39de6a299e
Renato Nitta <renatocn@gmail.com>
2013-11-19 18:21:00 -0200

    maçaroca de código que atende os requisitos de tracking de eventos para, links, botões, collapses e modais, agora falta refatorar

--- source/assets/javascripts/locastyle/_track_events.js
+++ source/assets/javascripts/locastyle/_track_events.js
@@ -4,6 +4,72 @@ locastyle.trackEvents = (function() {
   'use strict';
 
   function init(){
+    findLinks();
+    findButtons();
+  }
+
+  function findLinks(){
+    var links = $("a");
+    $(links).each(function (index, item) {
+      var options = {}
+      options.category = $("body").data("controller");
+      options.action = 'open_link';
+      if($(item).attr("href") === "#") {
+        options.action = 'on_page_link';
+        options.reload = true;
+      }
+      if($(item).data("toggle") === "modal"){
+        var modal = $(item).data("target") ? $(item).data("target") : $(item).attr("href");
+        options.action = 'open_modal_' + modal;
+      }
+      if($(item).data("toggle") === "collapse"){
+        options.type = "collapse";
+        options.action = 'open_collapse';
+      }
+      options.label = $(item).attr("title") ? $(item).attr("title") : $(item).text();
+      bindSendEvent(item, options);
+    });
+  }
+
+  function findButtons(){
+    var buttons = $("button");
+    $(buttons).each(function (index, item) {
+      var options = {}
+      options.category = $("body").data("controller");
+      options.action = 'open_button';
+      if($(item).attr("href") === "#") {
+        options.action = 'on_page_button';
+      }
+      if($(item).data("dismiss") === "modal"){
+        var modal = $($(item).parents(".modal")).attr("id");
+        options.action = 'close_modal_' + modal;
+      }
+      if($(item).data("toggle") === "dropdown"){
+        var modal = $($(item).parents(".modal")).attr("id");
+        options.action = 'dropdown_toggle';
+      }
+      options.label = $(item).attr("title") ? $(item).attr("title") : $(item).text();
+      bindSendEvent(item, options);
+    });
+  }
+
+
+  function bindSendEvent(element, options){
+    $(element).on("click", function () {
+      if(options.reload){
+        options.label = $(element).text();
+      }
+      if(options.type === "collapse"){
+        var targetCollapse = $(element).attr("href");
+        if($(targetCollapse).hasClass("in")){
+          options.action = 'close_collapse_' + targetCollapse;
+        } else {
+          options.action = 'open_collapse_' + targetCollapse;
+        }
+      }
+      ga('send', 'event', options.category, options.action, options.label);
+      console.log(options)
+    });
   }
 
   return {
