54df9b9b783b92a85690614802e1e95f8692d81a
Deivid Marques <deivid.goncalves@locaweb.com.br>
2013-11-21 18:01:26 -0200

    implementando o js inicial para ir direto ao conteudo via teclado

--- source/assets/javascripts/locastyle/_accessibility.js
+++ source/assets/javascripts/locastyle/_accessibility.js
@@ -4,8 +4,27 @@ locastyle.accessibility = (function() {
   'use strict';
 
   function init(){
+    areaAccess();
+    anchorContent();
   }
 
+  function areaAccess(){
+    $('.area-access a').on('focus', function(){
+      $(this).parent().addClass('in');
+    }).on('blur', function(){
+      $(this).parent().removeClass('in');
+    });
+  }
+
+  function anchorContent(){
+    $('.link-content').on('click',function(e){
+      e.preventDefault();
+      $('.title-content').attr('tabindex', '-1').focus().css('outline','none');
+      $('html, body').animate({
+        scrollTop: $('.title-content').offset().top
+      }, 500);
+    })
+  }
 
   return {
     init: init
