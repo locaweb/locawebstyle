3b783f4ff9f0bc4ca769986bc7b10f0b267045a6
Antonio Junior <antoniojunyor@sa10706.local>
2013-11-21 16:56:13 -0200

    Adicionando função que remove a classe active no elemento pai no collapse. #380

--- source/assets/javascripts/locastyle/_collapse_extends.js
+++ source/assets/javascripts/locastyle/_collapse_extends.js
@@ -5,12 +5,20 @@ locastyle.collapse = (function() {
 
   function init(){
     addClassParentCollapse();
+    removeClassParentCollapse();
   }
 
   // Função que adiciona classe no elemento pai do Collapse
   function addClassParentCollapse() {
     $('.collapse').on('show.bs.collapse', function () {
       $(this).parents('.collapse-box').addClass('active')
+    });
+  }
+
+  // Função que remove classe no elemento pai do Collapse
+  function removeClassParentCollapse() {
+    $('.collapse').on('hide.bs.collapse', function () {
+      $(this).parents('.collapse-box').removeClass('active')
     });
   }
 
