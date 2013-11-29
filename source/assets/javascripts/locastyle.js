7ea173a7aadcb6c67494f707cb10cf40e177dd2b
letanure <letanure@gmail.com>
2013-11-29 16:55:20 -0200

    padronizacao de nome de arquivos

--- source/assets/javascripts/locastyle.js
+++ source/assets/javascripts/locastyle.js
@@ -1,9 +1,9 @@
 //= require locastyle/_locastyle
 //= require locastyle/_initializer
 //= require locastyle/_analytics_trackevent
-//= require locastyle/_browser-detect
+//= require locastyle/_browser_detect
 //= require locastyle/_hopscotch
 //= require locastyle/_guided_tour
-//= require locastyle/_passwordStrength
-//= require locastyle/_step-form
+//= require locastyle/_password_strength
+//= require locastyle/_step_form
 //= require locastyle/_accessibility
