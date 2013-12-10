457ab984adf3bd0fdb9ca66ff7e87f912ffa61c0
letanure <letanure@gmail.com>
2013-12-10 15:19:29 -0200

    iniciando js de tabelas no locastyle

--- source/assets/javascripts/locastyle/_initializer.js
+++ source/assets/javascripts/locastyle/_initializer.js
@@ -2,6 +2,7 @@ $(window).load(function() {
   locastyle.init();
   locastyle.mobile.init();
   locastyle.bootstrap.init();
+  locastyle.tables.init();
   locastyle.passwordStregth.init();
   locastyle.carousel.init();
   locastyle.accessibility.init();
