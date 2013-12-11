1689195c55272d737af1264276ab17347d942712
letanure <letanure@gmail.com>
2013-12-11 02:43:47 -0200

    removi funcoes de tabela antigas, melhoria de estilo

--- source/assets/javascripts/locastyle/_tables.js
+++ source/assets/javascripts/locastyle/_tables.js
@@ -35,14 +35,14 @@ locastyle.tables = (function() {
       $checkboxes
         .prop('checked', evt.currentTarget.checked)
         .parents('tr').toggleClass('selected',  evt.currentTarget.checked );
-        toggleTableGroupActions($table, !evt.currentTarget.checked );
+        toggleTableGroupActions($table, (evt.currentTarget.checked ? $checkboxes.size() : 0 ) );
     });
     $checkboxes.on('change', function (evt) {
-      var checkeds = $checkboxes.filter(':checked').size();
-      var checkAllStatus = $checkboxes.size() === checkeds;
+      var checkeds = $checkboxes.filter(':checked').size(),
+          checkAllStatus = $checkboxes.size() === checkeds;
       $checkAll.prop('checked', checkAllStatus );
       $(this).parents('tr').toggleClass('selected',  evt.currentTarget.checked );
-      toggleTableGroupActions($table, checkeds < 2 );
+      toggleTableGroupActions($table, checkeds );
     });
   }
 
@@ -82,46 +82,11 @@ locastyle.tables = (function() {
     });
   }
 
-  function toggleTableGroupActions ($table, hidden) {
-    $table.prev('.ls-table-group-actions').toggleClass('hidden', hidden );
-  }
-
-  // Quando as tabelas tiverem checkboxes e mais de dois checkboxes forem marcados, será exibido um box com ações (ex: excluir, enviar, duplicar e etc).
-  function showActions () {
-    $('.ls-table').each(function() {
-
-      var $tableStyle = $(this);
-      var $wellTableId = $(this).prop('id');
-      var $checkAll   = $tableStyle.find('th input[type="checkbox"]');
-      var $checkboxes = $tableStyle.find('td input[type="checkbox"]');
-
-      // Quando clica no checkbox principal, seleciona todos os outros
-      $checkAll.on('change', function(){
-        $checkboxes.prop('checked', $checkAll.prop('checked') );
-        ( !$(this).prop('checked') ? $checkboxes.parents('tr').removeClass('selected') : $checkboxes.parents('tr').addClass('selected') );
-        showWellTable();
-      });
-
-      // Quando seleciona todos os checkboxes, seleciona o checkbox principal também
-      $checkboxes.on('change', function () {
-        $checkAll.prop('checked', $tableStyle.find('td input[type="checkbox"]:checked').size() == $checkboxes.size() );
-        $(this).parents('tr').toggleClass('selected');
-        showWellTable();
-      });
-
-      function showWellTable() {
-        // Verifica quantidade de itens checados para mostrar as opções de ação
-        if ( $tableStyle.find('td input[type="checkbox"]:checked').size() >= 1 ) {
-          $('[data-target="'+ $wellTableId +'"]').removeClass('hidden');
-        } else {
-          $('[data-target="'+ $wellTableId +'"]').addClass('hidden');
-        }
-
-        // Conta quantos checkboxes existe checados e marca no Counter
-        $('[data-target="'+ $wellTableId +'"]').find('.counterChecks').html($tableStyle.find('td input[type="checkbox"]:checked').size());
-      }
-
-    });
+  function toggleTableGroupActions ($table, checkeds) {
+    console.log( checkeds )
+    $table.prev('.ls-table-group-actions')
+      .toggleClass('hidden', checkeds < 2 )
+      .find('.counterChecks').text( checkeds )
   }
 
   return {
