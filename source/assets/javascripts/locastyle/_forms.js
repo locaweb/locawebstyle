fa36dc51cdf01740704213745ab8af2ee592e7de
letanure <letanure@gmail.com>
2013-12-11 15:38:43 -0200

    js modulo apenas de formularios

---
+++ source/assets/javascripts/locastyle/_forms.js
@@ -0,0 +1,15 @@
+var locastyle = locastyle || {};
+
+locastyle.forms = (function() {
+  'use strict';
+
+  var $forms = $('form', 'body');
+
+  function init(){
+  }
+
+  return {
+    init: init
+  };
+
+}());
