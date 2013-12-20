85fd5b3dc593f742418563750e256d6780f8b409
Renato Nitta <renatocn@gmail.com>
2013-12-20 08:27:47 -0200

    ativação/desativação de botões quando dentro de .btn-group

---
+++ spec/javascripts/locastyle_spec.js
@@ -0,0 +1,21 @@
+describe("Locastyle in general", function() {
+  beforeEach(function() {
+    loadFixtures('locastyle_general_fixture.html');
+    locastyle.init();
+  });
+
+  describe("Activation toggle", function () {
+    describe("When click on any .btn element inside an .activation-toggle", function() {
+      it("should remove any .active class from siblings", function() {
+        $("#activation_toggle_button_2").trigger("click");
+        expect($("#activation_toggle_button_1")).not.toHaveClass("active");
+      });
+
+      it("should add .active class on self", function() {
+        $("#activation_toggle_button_3").trigger("click");
+        expect($("#activation_toggle_button_3")).toHaveClass("active");
+      });
+    });
+  });
+
+});
