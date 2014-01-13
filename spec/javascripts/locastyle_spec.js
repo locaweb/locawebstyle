d020bc298fd71992e6611a0dcf68ffd5a9355876
letanure <letanure@gmail.com>
2014-01-13 20:12:42 -0200

    correcao testes locastyle geral - sem plugin

--- spec/javascripts/locastyle_spec.js
+++ spec/javascripts/locastyle_spec.js
@@ -6,16 +6,19 @@ describe("Locastyle in general", function() {
 
   describe("Activation toggle", function () {
     describe("When click on any .btn element inside an .activation-toggle", function() {
+      
       it("should remove any .active class from siblings", function() {
         $("#activation_toggle_button_2").trigger("click");
-        expect($("#activation_toggle_button_1")).not.toHaveClass("active");
+        expect( $("#activation_toggle_button_1").hasClass('active') ).toBeFalsy();
       });
 
       it("should add .active class on self", function() {
         $("#activation_toggle_button_3").trigger("click");
-        expect($("#activation_toggle_button_3")).toHaveClass("active");
+        expect( $("#activation_toggle_button_3").hasClass("active") ).toBeTruthy();
       });
+
     });
   });
 
 });
+Tr
