e8bc11fe8349ac9f81cedd5d9157085572907d0f
Deivid Marques <deivid.goncalves@locaweb.com.br>
2014-01-17 15:22:31 -0200

    inserindo testes na funcao de submenu

--- spec/javascripts/locastyle_spec.js
+++ spec/javascripts/locastyle_spec.js
@@ -4,9 +4,9 @@ describe("Locastyle in general", function() {
     locastyle.init();
   });
 
-  describe("Activation toggle", function () {
+  describe("Activation toggle", function() {
     describe("When click on any .btn element inside an .activation-toggle", function() {
-      
+
       it("should remove any .active class from siblings", function() {
         $("#activation_toggle_button_2").trigger("click");
         expect( $("#activation_toggle_button_1").hasClass('active') ).toBeFalsy();
@@ -17,6 +17,21 @@ describe("Locastyle in general", function() {
         expect( $("#activation_toggle_button_3").hasClass("active") ).toBeTruthy();
       });
 
+    });
+  });
+
+  describe("Submenu toggle", function(){
+    describe("When click on any element #linkSubmenu", function() {
+
+      it("should add .active class on self", function() {
+        $("#linkSubmenu").trigger("click");
+        expect( $("#linkSubmenu").hasClass("active") ).toBeTruthy();
+      });
+
+      it("should remove class .hidden in the element .submenu", function() {
+        $("#linkSubmenu").trigger("click");
+        expect( $(".submenu").hasClass('hidden') ).toBeFalsy();
+      });
     });
   });
 
