dfdf27e05533fefb1b17745595b5b100995b74a6
Luiz Tanure <letanure@gmail.com>
2014-01-22 12:37:39 -0200

    init testes tabelas

---
+++ spec/javascripts/tables_spec.js
@@ -0,0 +1,13 @@
+describe("Tables", function() {
+  beforeEach(function() {
+    loadFixtures('tables.html');
+    locastyle.tables.init();
+  });
+
+  describe("Simple tables", function () {
+  }); // Simples tables
+
+  describe("Complex tables", function () {
+  }); // Complex tables
+
+});
