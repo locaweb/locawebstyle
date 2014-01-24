6b96fe7201ec3c511bc50e2e695f63ad2203bc0b
Luiz Tanure <letanure@gmail.com>
2014-01-24 18:46:07 -0200

    configs, ajuste de altura e comportamento visual de seleção de item

--- source/assets/javascripts/locastyle/_sum_values.js
+++ source/assets/javascripts/locastyle/_sum_values.js
@@ -1,8 +1,40 @@
 Locastyle.prototype.sumValues = (function() {
   'use strict';
 
+  var config = {
+    selector: {
+      container: '.ls-sum-values-container',
+      item: '.sumItem',
+      elemTrigger: ':radio, :checkbox'
+    },
+    itemActiveClass: 'active'
+  }
+
   function init(){
-    console.log('ah lek')
+    $( config.selector.container ).each(function(i, sumContainer){
+      var $sumContainer = $(this)
+          $sumContainer = $(sumContainer);
+      $sumContainer.find(config.selector.item).each(function(i, itemSum){
+        var $itemSum = $(itemSum);
+        heightAdjust($itemSum);
+        selectItem($sumContainer, $itemSum);
+      });
+    })
+  }
+
+  function heightAdjust($itemSum){
+      $itemSum.children('span').height( $itemSum.height() -20 )
+      $itemSum.children('.sumValue').css( 'line-height' , $itemSum.height() -20 + 'px' );
+  }
+
+  function selectItem ($sumContainer, $item) {
+    $item.find( config.selector.elemTrigger ).on('change', function(evt){
+      var $itemSum = $(this).parents(config.selector.item),
+          $input = $(this);
+      $('[name="' + $input.attr('name') + '"]', $sumContainer ).parents(config.selector.item).removeClass(config.itemActiveClass);
+      $itemSum.toggleClass(config.itemActiveClass, $input.prop('checked') );
+
+    });
   }
 
   return {
