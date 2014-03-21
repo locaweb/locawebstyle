dc620a190ac15e43aaaf2fc05203dafe9ec21e0c
letanure <letanure@gmail.com>
2014-03-21 16:42:22 -0300

    opcoes de build

--- config.rb
+++ config.rb
@@ -52,16 +52,16 @@ set :images_dir, 'assets/images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
   # Use relative URLs
-  # activate :relative_assets
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
