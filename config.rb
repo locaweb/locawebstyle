f68b8ac48710ca324e571554832ac1266950600b
letanure <letanure@gmail.com>
2014-03-21 19:25:45 -0300

    layouts aninhados

--- config.rb
+++ config.rb
@@ -24,6 +24,9 @@
 #   page "/admin/*"
 # end
 
+page "/", :layout => "home"
+page "/documentacao/*", :layout => "documentacao"
+
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
