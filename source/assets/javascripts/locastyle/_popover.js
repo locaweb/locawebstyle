ad4d3bfa0a78a7201fadc31c31620175137812fe
Flavio Muniz <vitait.flavio.muniz@locaweb.com.br>
2014-03-25 19:55:43 -0300

    Fiz uns ajustes no modulo do popover e no template do mesmo

--- source/assets/javascripts/locastyle/_popover.js
+++ source/assets/javascripts/locastyle/_popover.js
@@ -8,6 +8,8 @@ locastyle.popover = (function() {
   }
 
   function getPopover(){
+    // Get all popover with data-toggle popover
+    // and send element and eventType to setAction() method
     $('[data-toggle="popover"]').each(function(index, element){
       var dataTrigger = $(element).data("trigger");
       var eventType = dataTrigger == 'hover' ? 'mouseenter' : 'click'
@@ -17,20 +19,31 @@ locastyle.popover = (function() {
 
   function setAction(element, eventType){
     $(element).on(eventType, function(event){
+      //Hide popover when init
+      hide();
+
       event.preventDefault();
       event.stopPropagation();
+
+      //Declare variables to data usage on popover
       var title = $(element).data("title");
       var content = $(element).data("content");
       var placement = $(element).data("placement");
-      locastyle.templates.popover($(element), title, content, placement)
+
+      //Return template popover
+      $(element).append(locastyle.templates.popover(title, content, placement));
+    }).on('mouseleave', function(){
+      //Hide popover when mouse out
+      hide();
     })
   }
 
   function show(){
+
   }
 
   function hide(){
-
+    $(".ls-popover").hide()
   }
 
   return {
