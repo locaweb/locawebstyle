bdfe6b4b3bd5631307cfe9b86792902e007fe712
Deivid Marques <deivid.goncalves@locaweb.com.br>
2014-03-26 16:49:12 -0300

    adicionando js q exibe sidebar no mobile

--- source/assets/javascripts/locastyle/_general.js
+++ source/assets/javascripts/locastyle/_general.js
@@ -4,10 +4,22 @@ locastyle.general = (function() {
 
   function init() {
 
-    $(config.selector).each(function(i, collapse){
-      console.log(collapse);
-    });
+    // $(config.selector).each(function(i, collapse){
+    //   console.log(collapse);
+    // });
 
+    showHide.addEventListener('click', showSidebar);
+  }
+
+  var showHide = document.querySelector('.ls-ico-menu'),
+  body = document.querySelector('body');
+
+  function showSidebar() {
+    if (body.className.match(/ls-show-sidebar/g)) {
+      body.classList.remove('ls-show-sidebar');
+    } else {
+      body.classList.add('ls-show-sidebar');
+    }
   }
 
   return {
