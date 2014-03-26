c758596089b61ba3e70c96470220555067edd96c
Flavio Muniz <vitait.flavio.muniz@locaweb.com.br>
2014-03-26 10:59:21 -0300

    Adicionando teste ao popover

---
+++ spec/javascripts/popover_spec.js
@@ -0,0 +1,22 @@
+describe("Popover: ", function(){
+  beforeEach(function(){
+    loadFixtures('popover_fixture.html');
+    locastyle.init($(document));
+    jasmine.clock().install();
+  });
+
+  afterEach(function() {
+    jasmine.clock().uninstall();
+  });
+
+
+  describe("When click on element with data-toggle[popover]", function(){
+    it("Should create a popover", function(){
+      $('[data-toggle="popover"]').trigger("click");
+      jasmine.clock().tick(1000);
+      console.log($(".ls-popover"))
+      expect($(".ls-popover")).toBeVisible();
+    });
+  });
+
+});
