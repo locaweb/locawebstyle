4f9097b2ed209e7421234bf43ae8e495f72f282a
Luiz Tanure <letanure@gmail.com>
2014-03-28 14:52:42 -0300

    eevents & load events

--- source/assets/javascripts/locastyle/_general.js
+++ source/assets/javascripts/locastyle/_general.js
@@ -2,9 +2,23 @@ var locastyle = locastyle || {};
 locastyle.general = (function() {
   'use strict';
 
+    var events = {
+  }
+
   function init() {
+    _loadEvents();
     showSidebar();
     subMenu();
+  }
+
+  function _loadEvents () {
+    $.each(events, function (eventDesc,fn) {
+      var selectorEvent = eventDesc.split('|')
+      $(selectorEvent[0]).on(selectorEvent[1], selectorEvent[2], function(evt){
+        var $this = $(this);
+        fn(evt, $this);
+      });
+    });
   }
 
   function showSidebar() {
