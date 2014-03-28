41d9a6341b52291d146741bfbab4763dd371ea8d
Renato Nitta <renatocn@gmail.com>
2014-03-28 09:52:16 -0300

    adding tabs files and first behaviour, target activation on trigger click

--- source/assets/javascripts/locastyle.js
+++ source/assets/javascripts/locastyle.js
@@ -4,5 +4,6 @@
 //= require locastyle/_modal
 //= require locastyle/_popover
 //= require locastyle/_tooltip
+//= require locastyle/_tabs
 //= require locastyle/_templates
 //= require locastyle/_general
