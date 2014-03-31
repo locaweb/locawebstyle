81e824a6a0ed384de0b0d7c1d3362a6b9235d9cd
Deivid Marques <deivid.goncalves@locaweb.com.br>
2014-03-31 17:38:31 -0300

    jasmine test on the activation button group

--- spec/javascripts/general_spec.js
+++ spec/javascripts/general_spec.js
@@ -1,10 +1,12 @@
 describe("Locastyle in general: ", function() {
 
+  beforeEach(function(){
+    loadFixtures('general_fixture.html');
+    locastyle.general.init();
+  });
+
+
   describe("Submenu Toggle ", function(){
-    beforeEach(function(){
-      loadFixtures('general_fixture.html');
-      locastyle.general.init();
-    });
 
     describe("When click on any link element of ls-submenu", function(){
       it("should add .active class on self", function(){
@@ -15,6 +17,22 @@ describe("Locastyle in general: ", function() {
       it("should remove .active class on self", function(){
         $(".ls-submenu.active > a").trigger("click");
         expect( $(".ls-submenu").hasClass("active") ).toEqual(false);
+      });
+
+    });
+  });
+
+  describe("Activation Group button toggle", function() {
+    describe("When click on any .btn element inside an .ls-group-active", function() {
+
+      it("should remove any .active class from siblings", function() {
+        $("#activation_toggle_button_2").trigger("click");
+        expect( $("#activation_toggle_button_1").hasClass('active') ).toEqual(false);
+      });
+
+      it("should add .active class on self", function() {
+        $("#activation_toggle_button_3").trigger("click");
+        expect( $("#activation_toggle_button_3").hasClass("active") ).toEqual(true);
       });
 
     });
