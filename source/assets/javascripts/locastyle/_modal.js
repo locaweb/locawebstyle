0901cb7b969b0c4b9c0e6a66463d439fab95879c
Flavio Muniz <vitait.flavio.muniz@locaweb.com.br>
2014-03-31 17:21:49 -0300

    Init modal

--- source/assets/javascripts/locastyle/_modal.js
+++ source/assets/javascripts/locastyle/_modal.js
@@ -2,12 +2,40 @@ var locastyle = locastyle || {};
 locastyle.modal = (function() {
   'use strict';
 
-  function init() {
+  var config = {
+    modal: '.ls-modal',
+    effetct: 'fade',
+    open: {
+      trigger: '[data-module="modal"]'
+    },
+    close: {
+      trigger: '[data-dismiss="modal"]'
+    }
+  }
 
+  function init() {
+    _open();
+    _close();
+  }
+
+  function _open(){
+    $(config.open.trigger).on('click', function(){
+      $('body').append(
+        $(config.modal).addClass("opened "+config.effetct).show()
+      );
+      $('body').append( '<div class="ls-modal-overlay"></div>');
+    })
+  }
+
+  function _close(){
+    $(config.close.trigger).on('click', function(){
+      $(config.modal).removeClass("opened").hide();
+      $(".ls-modal-overlay").remove()
+    })
   }
 
   return {
-    init:init
+    init: init
   };
 
 }());
