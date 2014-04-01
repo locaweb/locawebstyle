1bb29a17b7d8dd4655fd2e3d3831708260afcfbe
letanure <letanure@gmail.com>
2014-04-01 20:29:35 -0300

    markup menu & code automagically

--- source/assets/javascripts/docs/docs.js
+++ source/assets/javascripts/docs/docs.js
@@ -4,6 +4,7 @@ var lsdocs = (function() {
   function init() {
     convertCodeExamples();
     copyMarkup();
+    markupExamples();
   }
 
   function copyMarkup(){
@@ -25,8 +26,41 @@ var lsdocs = (function() {
 } );
   }
 
+  function markupExamples () {
+    $('.doc-example-markup').each(function (i, example) {
+      var $example = $(example);
+
+      // $(this).text(html);
+      // $(this).removeClass('language-html').addClass('language-markup');
+
+      $example
+        .append('<menu class="doc-example-menu"> <button class="doc-example-menu-demo doc-example-menu-active"> <i class="ls-ico-cog"></i> </button> <button class="doc-example-menu-code"> <i class="ls-ico-chevron-left"></i> <i class="ls-ico-chevron-right"></i> </button> </menu>')
+        .append('<div class="doc-example-markup-area"><code class="language-markup"></code></div>');
+      var html = $example.html();
+      $example.find('.doc-example-markup-area code').text(html);
+      // Prism.highlightAll();
+      menuExamples($example);
+    });
+  }
+
+  function menuExamples ($example) {
+    $example.find('.doc-example-menu button').on('click', function (evt) {
+      evt.preventDefault();
+      $example.find('.doc-example-menu-active').removeClass('doc-example-menu-active');
+      $(this).addClass('doc-example-menu-active');
+      if( $(this).hasClass('doc-example-menu-code')  ){
+        $example.find('.doc-example-markup-area').show()
+      }else{
+        $example.find('.doc-example-markup-area').hide()
+      }
+      // doc-example-menu-code
+      // doc-example-menu-demo
+
+    });
+  }
 
   function convertCodeExamples(){
+    return;
     $('code.language-html').each(function (i,e) {
       var html = $(this).html();
       $(this).text(html);
