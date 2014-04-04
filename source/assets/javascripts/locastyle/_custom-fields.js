871c44887ac45f2695fa7f4460b72afca0f17d2f
flaviomuniz <flaviomunizjr@gmail.com>
2014-04-04 19:19:26 -0300

    Custom checkbox field

--- source/assets/javascripts/locastyle/_custom-fields.js
+++ source/assets/javascripts/locastyle/_custom-fields.js
@@ -9,14 +9,26 @@ locastyle.customFields = (function() {
 
   function findFields(){
     $('.ls-form').find(':input').not('.ls-input-original').each(function(index, field){
+      $(field).wrap( '<div class="ls-field-container-' + $(field)[0].type + '" />' );
+
+      setSize($(field))
+
       if($(field)[0].type === 'select-one'){
         customSelect($(field));
       }
+
+      if($(field)[0].type === 'checkbox'){
+        customCheckbox($(field));
+      }
+
     })
   }
 
+  function setSize($field){
+    $(".ls-field-container-" + $field[0].type ).css( { 'width': $field.width(), 'height': $field.height() } )
+  }
+
   function customSelect($field){
-    $field.wrap( '<div class="ls-field-container-' + $field[0].type + '" />' );
     var selectText = $field.children('option:selected').eq(0).text();
     $('<span class="ls-field-custom-'+ $field[0].type +'"> ' + selectText + ' </span>').insertBefore($field[0]);
     var $labelField = $field.siblings('.ls-field-custom-'+ $field[0].type);
@@ -25,9 +37,16 @@ locastyle.customFields = (function() {
     })
   }
 
+  function customCheckbox($field){
+    $('<span class="ls-field-custom-'+ $field[0].type +'"></span>').insertBefore($field[0]);
+    var $labelField = $field.siblings('.ls-field-custom-'+ $field[0].type);
+    $field.on('click', function(){
+      $field.is(":checked") ? $labelField.addClass("checked") : $labelField.removeClass("checked")
+    })
+  }
+
   return {
-    init: init,
-    findFields: findFields
+    init: init
   }
 
 }());
