113f8cb5a59e24262d5aba2bb51e06e7f2fbd677
flaviomuniz <flaviomunizjr@gmail.com>
2014-04-04 19:26:17 -0300

    jasmine test of custom checkbox field

--- spec/javascripts/custom-fields_spec.js
+++ spec/javascripts/custom-fields_spec.js
@@ -6,7 +6,19 @@ describe('Custom Fields:', function(){
 
   describe('When have .ls-form', function(){
     it('all selects should have before element with .ls-field-custom-select-one', function(){
-      expect($('#myAwesomeId select').parent().find('.ls-field-custom-select-one')).toBeTruthy();
+      expect($('#myAwesomeSelect select').parent().find('.ls-field-custom-select-one')).toBeTruthy();
+    })
+
+    it('all checkboxes should have before element with .ls-field-custom-checkbox', function(){
+      expect($('#myAwesomeCheckbox select').parent().find('.ls-field-custom-checkbox')).toBeTruthy();
+    })
+
+  })
+
+  describe('When click on checkbox with .ls-form', function(){
+    it('should add class checked on .ls-field-custom-checkbox', function(){
+      $("#myAwesomeCheckbox input[type='checkbox']").trigger('click');
+      expect($('#myAwesomeCheckbox .ls-field-custom-checkbox')).toHaveClass('checked');
     })
   })
 
