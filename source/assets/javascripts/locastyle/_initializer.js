4cf613c30c90116c9b7a7e45676748bed630878e
Renato Nitta <renatocn@gmail.com>
2014-04-11 09:36:19 -0300

    improving initializer to call only present modules

--- source/assets/javascripts/locastyle/_initializer.js
+++ source/assets/javascripts/locastyle/_initializer.js
@@ -2,11 +2,21 @@ var locastyle = (function() {
   'use strict';
 
   function init() {
-    for (var module in locastyle) {
-      if (locastyle.hasOwnProperty(module)) {
-        module != 'init' ? locastyle[module].init() : null;
-      }
+    var modules = getModules();
+    for (var i in modules) {
+      locastyle[modules[i]].init();
     }
+  }
+
+  function getModules() {
+    var modules = [];
+    $("[data-ls-module]").each(function () {
+      var module = $(this).data("ls-module");
+      if ( !( modules.indexOf(module) > -1 ) ) {
+        modules.push(module);
+      }
+    })
+    return modules;
   }
 
   return {
