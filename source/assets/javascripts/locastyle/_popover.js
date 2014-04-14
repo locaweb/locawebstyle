820cb5076416af48fe349b61065ad24bf38da275
flaviomuniz <flaviomunizjr@gmail.com>
2014-04-14 17:46:22 -0300

    Reindent code and create unbind method for popover

--- source/assets/javascripts/locastyle/_popover.js
+++ source/assets/javascripts/locastyle/_popover.js
@@ -8,16 +8,18 @@ locastyle.popover = (function() {
     defaultContainer: 'body',
     defaultTrigger: 'click',
     defaultPlacement: 'top',
-    popoverClass: '.ls-popover'
+    popoverClass: '.ls-popover',
+    trigger: '[data-ls-module="popover"]'
   }
 
   function init() {
-    $('[data-ls-module="popover"]').each(function(index, element) {
+    unbind();
+    $(config.trigger).each(function(index, element) {
       var dataTrigger = $(element).data("trigger");
       dataTrigger = dataTrigger || config.defaultTrigger
       bindAction({
         'element': element,
-        'eventType': dataTrigger == 'hover' ? 'mouseenter' : 'click'
+        'eventType': dataTrigger == 'hover' ? 'mouseenter.ls' : 'click.ls'
       });
     });
   }
@@ -51,30 +53,30 @@ locastyle.popover = (function() {
     // Return template popover
     $(elementData.container).append(locastyle.templates.popover(elementData));
 
-    unBindClick(element);
+    unbindPopoverTrigger(element);
     getTriggerPosition(elementData);
   }
 
   function getTriggerPosition(elementPosition) {
     var left,
-      top,
-      leftPlacement,
-      setSide,
-      setTop,
-      topPlacement
-      var element = elementPosition.element
-      var placement = elementPosition.placement
-      var container = elementPosition.container
-      var elementWidth = $(element).outerWidth();
-      var elementHeight = $(element).outerHeight();
+    top,
+    leftPlacement,
+    setSide,
+    setTop,
+    topPlacement
+    var element = elementPosition.element
+    var placement = elementPosition.placement
+    var container = elementPosition.container
+    var elementWidth = $(element).outerWidth();
+    var elementHeight = $(element).outerHeight();
 
-      if (container == 'body') {
-        top = $(element).offset().top;
-        left = $(element).offset().left;
-      } else {
-        top = $(element).position().top;
-        left = $(element).position().left;
-      }
+    if (container == 'body') {
+      top = $(element).offset().top;
+      left = $(element).offset().left;
+    } else {
+      top = $(element).position().top;
+      left = $(element).position().left;
+    }
 
 
     var setSide = left;
@@ -85,97 +87,103 @@ locastyle.popover = (function() {
       setTop = top
     }
 
-      if (placement == 'bottom') {
-        setTop = (top + elementHeight)
-      }
+    if (placement == 'bottom') {
+      setTop = (top + elementHeight)
+    }
 
-      if (placement == 'left') {
-        leftPlacement = true;
-        setSide = left
-      }
+    if (placement == 'left') {
+      leftPlacement = true;
+      setSide = left
+    }
 
-      if (placement == 'right') {
-        setSide = (left + elementWidth)
-      }
+    if (placement == 'right') {
+      setSide = (left + elementWidth)
+    }
 
-      setPopoverPosition({
-        'setTop': setTop,
-        'placement': placement,
-        'setSide': setSide,
-        'leftPlacement': leftPlacement,
-        'topPlacement': topPlacement,
-        'elementWidth': elementWidth,
-        'elementHeight': elementHeight
-      });
-      }
+    setPopoverPosition({
+      'setTop': setTop,
+      'placement': placement,
+      'setSide': setSide,
+      'leftPlacement': leftPlacement,
+      'topPlacement': topPlacement,
+      'elementWidth': elementWidth,
+      'elementHeight': elementHeight
+    });
+  }
 
-    function setPopoverPosition(popoverPosition) {
+  function setPopoverPosition(popoverPosition) {
 
-      var positionWithTop = (popoverPosition.setTop - $(config.popoverClass).height() / 2 + popoverPosition.elementHeight / 2);
-      var positionWithLeft = (popoverPosition.setSide - $(config.popoverClass).width() / 2 + popoverPosition.elementWidth / 2);
+    var positionWithTop = (popoverPosition.setTop - $(config.popoverClass).height() / 2 + popoverPosition.elementHeight / 2);
+    var positionWithLeft = (popoverPosition.setSide - $(config.popoverClass).width() / 2 + popoverPosition.elementWidth / 2);
 
-      var _default = {
-        top: {
-          css: 'top',
-          value: positionWithTop,
-          adjust: {
-            'left': positionWithLeft
-          },
-          add: {
-            'top':( popoverPosition.setTop - $(config.popoverClass).height())
-          }
+    var _default = {
+      top: {
+        css: 'top',
+        value: positionWithTop,
+        adjust: {
+          'left': positionWithLeft
         },
-        right: {
-          css: 'left',
-          value:( popoverPosition.setSide),
-          adjust: {
-            'top': positionWithTop
-          }
+        add: {
+          'top':( popoverPosition.setTop - $(config.popoverClass).height())
+        }
+      },
+      right: {
+        css: 'left',
+        value:( popoverPosition.setSide),
+        adjust: {
+          'top': positionWithTop
+        }
+      },
+      bottom: {
+        css: 'top',
+        value:( popoverPosition.setTop),
+        adjust: {
+          'left': positionWithLeft
+        }
+      },
+      left: {
+        css: 'left',
+        value:( popoverPosition.setSide - $(config.popoverClass).width()),
+        adjust: {
+          'top': positionWithTop
         },
-        bottom: {
-          css: 'top',
-          value:( popoverPosition.setTop),
-          adjust: {
-            'left': positionWithLeft
-          }
-        },
-        left: {
-          css: 'left',
-          value:( popoverPosition.setSide - $(config.popoverClass).width()),
-          adjust: {
-            'top': positionWithTop
-          },
-          add: {
-            'left':( popoverPosition.setSide - $(config.popoverClass).width()),
-            'top': positionWithTop
-          }
+        add: {
+          'left':( popoverPosition.setSide - $(config.popoverClass).width()),
+          'top': positionWithTop
         }
       }
+    }
 
-      $(config.popoverClass)
-        .css(_default[popoverPosition.placement].css, _default[popoverPosition.placement].value)
-        .css(_default[popoverPosition.placement].adjust);
-      if (_default[popoverPosition.placement].add) {
-        $(config.popoverClass).css(_default[popoverPosition.placement].add);
-      }
-      }
+    $(config.popoverClass)
+    .css(_default[popoverPosition.placement].css, _default[popoverPosition.placement].value)
+    .css(_default[popoverPosition.placement].adjust);
+    if (_default[popoverPosition.placement].add) {
+      $(config.popoverClass).css(_default[popoverPosition.placement].add);
+    }
+  }
 
-    function unBindClick(element) {
-      if ($(config.popoverClass).is(":visible")) {
-        $(element).on('click', function() {
-          destroyPopover();
-          init();
-        })
-      }
-      }
+  function unbindPopoverTrigger(element) {
+    if ($(config.popoverClass).is(":visible")) {
+      $(element).on('click.ls', function() {
+        destroyPopover();
+        init();
+      })
+    }
+  }
 
-    function destroyPopover() {
-      $(config.popoverClass).remove()
-      }
+  function destroyPopover() {
+    $(config.popoverClass).remove()
+  }
 
-    return {
-      init: init,
-      destroyPopover: destroyPopover
-      }
+  function unbind(){
+    $(config.trigger).off('click.ls')
+    $(config.trigger).off('mouseenter.ls')
+  }
 
-    }());
+  return {
+    init: init,
+    destroyPopover: destroyPopover,
+    unbind: unbind
+  }
+
+}());
