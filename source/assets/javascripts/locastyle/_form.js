d55af0cf41aae171b1f5344e3698d87435f73997
letanure <letanure@gmail.com>
2014-04-14 20:52:41 -0300

    mask shortcuts

--- source/assets/javascripts/locastyle/_form.js
+++ source/assets/javascripts/locastyle/_form.js
@@ -14,7 +14,22 @@ locastyle.form = (function() {
 
     formDisable();
     formText();
+    masks();
 
+  }
+
+  function masks () {
+    $('.ls-mask-date').mask('11/11/1111');
+    $('.ls-mask-time').mask('00:00:00');
+    $('.ls-mask-date_time').mask('00/00/0000 00:00:00');
+    $('.ls-mask-cep').mask('00000-000');
+    $('.ls-mask-phone').mask('0000-0000');
+    $('.ls-mask-phone_with_ddd').mask('(00) 0000-0000');
+    $('.ls-mask-mixed').mask('AAA 000-S0S');
+    $('.ls-mask-cpf').mask('000.000.000-00', {reverse: true});
+    $('.ls-mask-ip_address').mask('0ZZ.0ZZ.0ZZ.0ZZ', {translation: {'Z': {pattern: /[0-9]/, optional: true}}});
+    $('.ls-mask-percent').mask('##0,00%', {reverse: true});
+    );
   }
 
   function formDisable () {
