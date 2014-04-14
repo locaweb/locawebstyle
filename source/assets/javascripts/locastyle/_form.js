e99665c35bc0e1d6220191226bd4a2a117d05e89
Luiz Tanure <letanure@gmail.com>
2014-04-14 19:28:49 -0300

    form as text

--- source/assets/javascripts/locastyle/_form.js
+++ source/assets/javascripts/locastyle/_form.js
@@ -5,21 +5,30 @@ locastyle.form = (function() {
 
   var config = {
     selectors: {
-      disable: '.ls-form-disable'
+      disable: '.ls-form-disable',
+      text: '.ls-form-text'
     }
   }
 
   function init() {
 
     formDisable();
+    formText();
 
   }
 
   function formDisable () {
     $( config.selectors.disable ).each(function(ic, container){
       $(container).find(':input').each(function(ie, field){
-        console.log( $(field) )
         $(field).attr('disabled', 'disabled')
+      });
+    });
+  }
+  function formText ()
+  {
+    $( config.selectors.text ).each(function(ic, container){
+      $(container).find(':input').each(function(ie, field){
+        $(field).addClass('ls-form-text');
       });
     });
   }
