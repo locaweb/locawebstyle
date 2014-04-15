d7cc91ee93957a4fa49aedad67f053814927d798
flaviomuniz <flaviomunizjr@gmail.com>
2014-04-15 15:16:06 -0300

    fix jarmine for custom select

--- spec/javascripts/custom-fields_spec.js
+++ spec/javascripts/custom-fields_spec.js
@@ -4,14 +4,9 @@ describe('Custom Fields:', function(){
     locastyle.customFields.init();
   });
 
-  describe('When have .ls-custom', function(){
+  describe('When have data-ls-module="customFields"', function(){
     it('all selects should have before element with .ls-field-custom-select-one', function(){
       expect($('#myAwesomeSelect select').parent().find('.ls-field-custom-select-one')).toBeTruthy();
     })
-
-    it('all checkboxes should have before element with .ls-field-custom-checkbox', function(){
-      expect($('#myAwesomeCheckbox select').parent().find('.ls-field-custom-checkbox')).toBeTruthy();
-    })
-
   })
 });
