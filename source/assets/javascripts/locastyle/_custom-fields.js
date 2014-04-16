d91d01038221d72353b2056349e135e6398e7251
Diego Eis <diegoeis@gmail.com>
2014-04-16 14:31:34 -0300

    deleting a lot of functions in custom selects, resolving some issues in custom select style. with @flaviomuniz

--- source/assets/javascripts/locastyle/_custom-fields.js
+++ source/assets/javascripts/locastyle/_custom-fields.js
@@ -5,100 +5,36 @@ locastyle.customFields = (function() {
 
   function init() {
     findFields();
-    bindRemoveCustomClass();
   }
 
   function findFields(){
     $('[data-ls-module="customFields"]').find(':input').not('.ls-input-original').each(function(index, field){
       var fieldType = $(field)[0].type;
-      if(fieldType === 'select-one'){
-        if($(field).eq(0).attr('class') !== undefined){
-          $(field).wrap( '<div class="ls-field-container-' + fieldType + ' ' + $(field).eq(0).attr('class') + ' " />' );
-        }else{
-          $(field).wrap( '<div class="ls-field-container-' + fieldType + ' " />' );
-        }
-      }
+      var classParent = $(field).eq(0).attr('class');
+      classParent = classParent != undefined ? classParent : ''
 
-      setSize($(field))
+      $(field).wrap( '<div class="ls-custom-' + fieldType + ' ' + classParent + '" />' );
 
-      if($(field)[0].type === 'select-one'){ customSelect($(field)); }
-
-      // if($(field)[0].type === 'checkbox'){ customCheckbox($(field)); checkInputs($(field))}
-
-      // if($(field)[0].type === 'radio'){ customRadio($(field)); checkInputs($(field)) }
-
-    })
-  }
-
-  function setSize($field){
-    $(".ls-field-container-" + $field[0].type ).css( { 'width': $field.width() } );
+      fieldSpan($(field));
+    });
   }
 
   function fieldSpan($field){
-    $('<span class="ls-field-custom-'+ $field[0].type + '"></span>').insertBefore($field[0]);
+    var selectText = $field.children('option:selected').eq(0).text();
+    $('<span class="ls-field-custom-'+ $field[0].type + '">' + selectText + '</span>').insertAfter($field[0]);
+    bindCustomSelect($field)
   }
 
   function siblingsField($field){
     return $field.siblings('.ls-field-custom-'+ $field[0].type);
   }
 
-  // Select
-  function customSelect($field){
-    var selectText = $field.children('option:selected').eq(0).text();
-    $('<span class="ls-field-custom-'+ $field[0].type +'"> ' + selectText + ' </span>').insertBefore($field[0]);
-    bindCustomSelect($field)
-  }
-
   function bindCustomSelect($field){
     var $siblingsField = siblingsField($field)
     $field.on('change', function(){
       $siblingsField.text($field.find('option:selected').text())
-    })
-  }
-
-  // Checkbox
-  function customCheckbox($field){
-    fieldSpan($field);
-    bindCustomCheckbox($field)
-  }
-
-  function bindCustomCheckbox($field){
-    var $siblingsField = siblingsField($field)
-    $field.on('click', function(){
-      $field.is(":checked") ? $siblingsField.addClass("checked") : $siblingsField.removeClass("checked")
-    })
-  }
-  // Radio
-  function customRadio($field){
-    fieldSpan($field);
-    bindCustomRadio($field);
-  }
-
-  function bindCustomRadio($field){
-    $field.on('click', function(){
-      uncheckInputs($field);
-      checkInputs($field)
     });
   }
-
-  function uncheckInputs($field){
-    var group = $field.attr("name");
-    $field.parents().find('.ls-field-custom-radio').removeClass("checked");
-  }
-
-  function checkInputs($field){
-    var $siblingsField = siblingsField($field);
-    if($field.is(":checked")){
-      $siblingsField.addClass("checked")
-    }
-  }
-
-  function bindRemoveCustomClass(){
-    $('.ls-input-original').on('click', function(){
-      $('.ls-field-custom-radio').removeClass("checked");
-    });
-  }
-
 
   return {
     init: init
