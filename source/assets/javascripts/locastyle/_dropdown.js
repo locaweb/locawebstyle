7075b6bdfe0605040d9374ac8960d1c1a45619c1
Renato Nitta <renatocn@gmail.com>
2014-04-17 19:15:19 -0300

    adding unbind events on dropdown module

--- source/assets/javascripts/locastyle/_dropdown.js
+++ source/assets/javascripts/locastyle/_dropdown.js
@@ -4,23 +4,29 @@ locastyle.dropdown = (function() {
   'use strict';
 
   function init() {
+    unbind();
     bindClickOnTriggers();
     bindClickOutsideTriggers();
   }
 
+  function unbind() {
+    $("[data-ls-module=dropdown] > a:first-child").off("click.ls");
+    $("body").off("click.ls");
+  }
+
   function bindClickOnTriggers() {
-    $("[data-ls-module=dropdown] > a:first-child").on("click", function(evt) {
+    $("[data-ls-module=dropdown] > a:first-child").on("click.ls", function(evt) {
       evt.preventDefault();
       var $target = $($(this).parents("[data-ls-module=dropdown]"));
-      toggleDropdown($target);
-      closeDropdown($target);
+      locastyle.dropdown.toggleDropdown($target);
+      locastyle.dropdown.closeDropdown($target);
       evt.stopPropagation();
     });
   }
 
   function bindClickOutsideTriggers() {
-    $("body").on("click", function(){
-      closeDropdown();
+    $("body").on("click.ls", function(){
+      locastyle.dropdown.closeDropdown();
     });
   }
 
@@ -28,13 +34,15 @@ locastyle.dropdown = (function() {
     $target.toggleClass("active");
   }
 
-
   function closeDropdown(el) {
     $("[data-ls-module=dropdown]").not(el).removeClass("active");
   }
 
   return {
-    init: init
+    init: init,
+    unbind: unbind,
+    toggleDropdown: toggleDropdown,
+    closeDropdown: closeDropdown
   }
 
 }());
--- spec/javascripts/dropdown_spec.js
+++ spec/javascripts/dropdown_spec.js
@@ -39,4 +39,39 @@ describe("Dropdown: ", function() {
 
   });
 
+  describe("Unbind:", function() {
+    describe("when unbind is called in module init", function() {
+      it("should prevent toggleDropdown from being called twice or more times", function() {
+        var spy = spyOn(locastyle.dropdown, "toggleDropdown");
+        locastyle.dropdown.init();
+        locastyle.dropdown.init();
+        $("#dropdown-test > a:first-child").trigger("click");
+        expect(locastyle.dropdown.toggleDropdown.calls.count()).toEqual(1);
+      });
+
+      it("should prevent closeDropdown from being called twice or more times", function() {
+        var spy = spyOn(locastyle.dropdown, "closeDropdown");
+        locastyle.dropdown.init();
+        locastyle.dropdown.init();
+        $("#fake-body").trigger("click");
+        expect(locastyle.dropdown.closeDropdown.calls.count()).toEqual(1);
+      });
+
+      it("should NOT unbind common events handled by other code", function() {
+        window.test = {
+          method: function(){
+            //my fake function
+          }
+        };
+        var spy = spyOn(window.test, "method");
+        $("#dropdown-test > a:first-child").on("click", function () {
+          window.test.method();
+        })
+        locastyle.dropdown.unbind();
+        $("#dropdown-test > a:first-child").trigger("click");
+        expect(window.test.method).toHaveBeenCalled();
+      });
+    });
+  });
+
 });
