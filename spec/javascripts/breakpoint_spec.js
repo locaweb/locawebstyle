cfa9ad386cfc914754a8306aa6de44e6d8883e17
Wylkon Queiroz <vitait.wylkon.queiroz@locaweb.com.br>
2014-04-25 15:09:04 -0300

    Implemented breackpoints tests - in pair programming with Diego Eis

--- spec/javascripts/breakpoint_spec.js
+++ spec/javascripts/breakpoint_spec.js
@@ -16,4 +16,62 @@ describe("Breakpoint: ", function() {
     });
   });
 
+  describe("Resizing Window", function() {
+
+    beforeEach(function(){
+      loadFixtures('breakpoint_fixture.html');
+    });
+
+    afterEach(function(){
+      $('html').removeClass();
+    });
+
+    describe("HTML tag should has class", function() {
+
+      it("ls-screen-xs", function() {
+        locastyle.breakpoints.init({ documentWidth: 767 });
+        expect($('html').hasClass('ls-screen-xs')).toBeTruthy();
+      });
+
+      it("ls-screen-sm", function() {
+        locastyle.breakpoints.init({ documentWidth: 768 });
+        expect($('html').hasClass('ls-screen-sm')).toBeTruthy();
+      });
+
+      it("ls-screen-md", function() {
+        locastyle.breakpoints.init({ documentWidth: 992 });
+        expect($('html').hasClass('ls-screen-md')).toBeTruthy();
+      });
+
+      it("ls-screen-lg", function() {
+        locastyle.breakpoints.init({ documentWidth: 1200 });
+        expect($('html').hasClass('ls-screen-lg')).toBeTruthy();
+      });
+    });
+
+    describe("locastyle.breackpoint class value should has", function() {
+
+      it("ls-screen-xs", function() {
+        locastyle.breakpoints.init({ documentWidth: 767 });
+        expect(locastyle.breakpointClass).toEqual('ls-screen-xs');
+      });
+
+      it("ls-screen-sm", function() {
+        locastyle.breakpoints.init({ documentWidth: 768 });
+        expect(locastyle.breakpointClass).toEqual('ls-screen-sm');
+      });
+
+      it("ls-screen-md", function() {
+        locastyle.breakpoints.init({ documentWidth: 992 });
+        expect(locastyle.breakpointClass).toEqual('ls-screen-md');
+      });
+
+      it("ls-screen-lg", function() {
+        locastyle.breakpoints.init({ documentWidth: 1200 });
+        expect(locastyle.breakpointClass).toEqual('ls-screen-lg');
+      });
+
+    });
+  });
+
 });
