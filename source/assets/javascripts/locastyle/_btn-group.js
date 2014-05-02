4153dcb57b05365d8c707944284c9656ca45a186
Flavio Muniz <flaviomunizjr@gmail.com>
2014-05-02 11:48:42 -0300

    Regroup buttons if your parent has class .ls-regroup and transform in a dropdown

--- source/assets/javascripts/locastyle/_btn-group.js
+++ source/assets/javascripts/locastyle/_btn-group.js
@@ -4,17 +4,32 @@ locastyle.btnGroup = (function() {
 
   function init() {
     checkBreakpoint();
+    bindBreakpointUpdateOnChecker();
   }
 
+  // adiciona o bind de breakpoint-updated e chama o checker quando o evento ocorre
+  function bindBreakpointUpdateOnChecker() {
+    $(document).on("breakpoint-updated", function () {
+      checkBreakpoint();
+    })
+  }
+
+  // verifica se o breakpoint é para mobile, se for faz a ação para agrupar
   function checkBreakpoint() {
-    $(".ls-regroup").each(function (index, $element) {
-      group($($element).find('[class*="ls-btn"]'));
-    });
+    if(locastyle.breakpointClass == "ls-screen-sm" || locastyle.breakpointClass == "ls-screen-xs"){
+      $(".ls-regroup").each(function (index, $element) {
+        group($($element).find('[class*="ls-btn"]'));
+      });
+    }
   }
 
+  // agrupa os botões em um dropdown
   function group($element){
-    var list = $($element).wrap('<li></li>');
+    var list = $($element).wrap('<li class="hidden-xs hidden-sm">');
     $(".ls-regroup").append(locastyle.templates.dropdown(list));
+
+    // para funcionar o dropdown
+    locastyle.dropdown.init()
   }
 
   return {
