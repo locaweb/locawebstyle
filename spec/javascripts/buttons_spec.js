697f0a29e33c29ed2c48f8c8be30d2ad351734c9
flaviomuniz <flaviomunizjr@gmail.com>
2014-05-05 13:35:31 -0300

    Buttons regroup test

--- spec/javascripts/buttons_spec.js
+++ spec/javascripts/buttons_spec.js
@@ -1,15 +1,16 @@
-describe("Buttons group: ", function() {
-  beforeEach(function() {
+describe("Buttons group: ", function(){
+  beforeEach(function(){
     loadFixtures('buttons_fixture.html');
     locastyle.btnGroup.init();
   });
 
   describe("when html has class .ls-screen-sm or .ls-screen-xs", function(){
     it("all buttons into div with class .ls-regroup transform self in dropdown", function(){
-      $("html").addClass("ls-screen-xs")
-      locastyle.btnGroup.init()
-      expect($("#myGroupButtons").hasClass("ls-regroup")).toBe(true);
+      locastyle.breakpointClass = "ls-screen-xs";
+      locastyle.btnGroup.init();
+      expect($("#myButtonInDropdown").closest(".ls-dropdown").hasClass("ls-dropdown")).toBe(true);
     })
+
   })
 
 });
--- spec/javascripts/libs/templates.js
+++ spec/javascripts/libs/templates.js
@@ -58,3 +58,67 @@
     return __out.join('');
   };
 }).call(this);
+(function() { this.JST || (this.JST = {}); this.JST["locastyle/templates/_dropdown"] = function(__obj) {
+    if (!__obj) __obj = {};
+    var __out = [], __capture = function(callback) {
+      var out = __out, result;
+      __out = [];
+      callback.call(this);
+      result = __out.join('');
+      __out = out;
+      return __safe(result);
+    }, __sanitize = function(value) {
+      if (value && value.ecoSafe) {
+        return value;
+      } else if (typeof value !== 'undefined' && value != null) {
+        return __escape(value);
+      } else {
+        return '';
+      }
+    }, __safe, __objSafe = __obj.safe, __escape = __obj.escape;
+    __safe = __obj.safe = function(value) {
+      if (value && value.ecoSafe) {
+        return value;
+      } else {
+        if (!(typeof value !== 'undefined' && value != null)) value = '';
+        var result = new String(value);
+        result.ecoSafe = true;
+        return result;
+      }
+    };
+    if (!__escape) {
+      __escape = __obj.escape = function(value) {
+        return ('' + value)
+          .replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;');
+      };
+    }
+    (function() {
+      (function() {
+        var element, _i, _len, _ref;
+
+        __out.push('<div data-ls-module="dropdown" class="ls-dropdown">\n  <a href="#" class="ls-btn"></a>\n  <ul>\n    ');
+
+        _ref = this.elements;
+        for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+          element = _ref[_i];
+          __out.push('\n      ');
+          if (element.text !== '') {
+            __out.push('\n        <li>');
+            __out.push(element.outerHTML);
+            __out.push('</li>\n      ');
+          }
+          __out.push('\n    ');
+        }
+
+        __out.push('\n  </ul>\n</div>\n');
+
+      }).call(this);
+
+    }).call(__obj);
+    __obj.safe = __objSafe, __obj.escape = __escape;
+    return __out.join('');
+  };
+}).call(this);
