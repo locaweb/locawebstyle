0aa2499323687594d43ac7e1c4c1aef6f677da02
Wylkon Queiroz <wylkon@wylkon.com.br>
2014-06-03 14:50:48 -0300

    Implemented custom fields test.

--- spec/javascripts/custom-fields_spec.js
+++ spec/javascripts/custom-fields_spec.js
@@ -8,5 +8,13 @@ describe('Custom Fields:', function(){
     it('all selects should have before element with .ls-field-custom-select-one', function(){
       expect($('#myAwesomeSelect select').parent().find('.ls-field-custom-select-one')).toBeTruthy();
     })
-  })
+  });
+
+  describe("Unbind", function() {
+  	it("should bind change only one time on the element", function() {
+  		locastyle.customFields.init();
+  		locastyle.customFields.init();
+  		expect($('#trueId')).toHaveBeenBindedOnce('change');
+  	});
+  });
 });
