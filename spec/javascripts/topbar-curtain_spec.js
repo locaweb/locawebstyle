1dfc48c77c6ef95d9583fc565a8df7122de53e0d
Wylkon Queiroz <wylkon@wylkon.com.br>
2014-06-13 17:55:10 -0300

    Adjust test for topbar-curtain.

--- spec/javascripts/topbar-curtain_spec.js
+++ spec/javascripts/topbar-curtain_spec.js
@@ -8,10 +8,8 @@ describe("Topbar Curtain:", function() {
     it("should position the target element", function() {
       $("#link-clicked").css({position: "absolute", left: 600});
       $("#item-1").css({position: "absolute", left: 800, width: 280});
-      var expectedPosition = ($("#link-clicked").position().left - $(window).width()) - ($("#item-1").width()/2) + (22/2);
+      var expectedPosition = ($("#link-clicked").position().left) + ($("#item-1").width()/2) + (22/2);
       locastyle.topbarCurtain.init();
-      // pending until Wylkon helps us to know if it is related with recent fixes for IE
-      pending();
       expect($("#item-1").position().left).toEqual(expectedPosition);
     });
   });
