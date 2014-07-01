4f955170d3ae624475993b781111a00ae0b094ed
flaviomuniz <flaviomunizjr@gmail.com>
2014-07-01 14:30:31 -0300

    Improvements and create a test.

---
+++ spec/javascripts/counter_spec.js
@@ -0,0 +1,23 @@
+describe("Counter char: ", function(){
+  beforeEach(function(){
+    loadFixtures('counter_fixture.html');
+    locastyle.charCounter.init();
+  });
+
+  describe("When input or textarea call module charCounter and attribute maxlength", function(){
+    it("should create a element with .ls-char-count after input", function(){
+      expect($("#myCounter input").parent().find('.ls-char-count')).toExist();
+    });
+
+    it("should when keypress, decrease number of limit", function(){
+      var event = $.Event("keyup");
+      var $inputMaxlength = $("#myCounterTrigger input")
+      event.which = 65;
+      $inputMaxlength.trigger(event);
+      expect($("#myCounterTrigger").find('.ls-char-count').text()).toEqual('6');
+    })
+
+  })
+
+
+});
