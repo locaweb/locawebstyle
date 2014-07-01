71bce40c8ed29e2dac7e18a8b1ac8642144bda27
Renato Nitta <renatocn@gmail.com>
2014-07-01 14:22:19 -0300

    added common links to event tracking

--- spec/javascripts/track-events_spec.js
+++ spec/javascripts/track-events_spec.js
@@ -1,6 +1,7 @@
 describe("Track Events: ", function() {
   beforeEach(function() {
     loadFixtures('track-events_fixture.html');
+    $("body").data("ls-te-category", "locastyle#track-events-test");
     locastyle.trackEvents.init();
   });
 
@@ -14,6 +15,21 @@ describe("Track Events: ", function() {
         window.ga = null;
         locastyle.trackEvents.init($(document));
         expect(locastyle.trackEvents.gaPresent).toEqual(false);
+      });
+    });
+  });
+
+  describe("Common links", function () {
+    describe("When click on link on #home_menu_sample", function () {
+      it("should call ga with ('send', 'event', 'locastyle#track-events-test', 'open_link_#/home', 'Open link sample') arguments", function () {
+        var expectedOptions = {
+          category: "locastyle#track-events-test",
+          action: "open_link_#/home",
+          label: "Open link sample"
+        }
+        spyOn(window, "ga");
+        $("#open_links #home_menu_sample").trigger("click.ls");
+        expect(window.ga).toHaveBeenCalledWith('send', 'event', expectedOptions.category, expectedOptions.action, expectedOptions.label);
       });
     });
   });
