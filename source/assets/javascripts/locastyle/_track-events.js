c94aea3b83864182f8da76507e88d1b9085f7525
Renato Nitta <renatocn@gmail.com>
2014-07-01 14:22:19 -0300

    added common links to event tracking

--- source/assets/javascripts/locastyle/_track-events.js
+++ source/assets/javascripts/locastyle/_track-events.js
@@ -6,9 +6,31 @@ locastyle.trackEvents = (function() {
   function init(){
     if(window.ga){
       this.gaPresent = true;
+      findTriggers();
     } else {
       this.gaPresent = false;
     }
+  }
+
+  function findTriggers(){
+    findLinks();
+  }
+
+  function findLinks(){
+    var links = $("a");
+    $(links).each(function (index, item) {
+      var options = {}
+      options.category = $("body").data("ls-te-category");
+      options.action = 'open_link_#' + $(item).attr("href");
+      options.label = $(item).text();
+      bindClickEvents(item, options);
+    });
+  }
+
+  function bindClickEvents(element, options){
+    $(element).on("click.ls", function () {
+      ga('send', 'event', options.category, options.action, options.label);
+    });
   }
 
   return {
