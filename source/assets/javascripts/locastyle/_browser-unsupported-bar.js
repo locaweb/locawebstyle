2fc5dd0333153b57e2e65fb58910d02e716d2156
Antonio Junior <junyor@hotmail.co.uk>
2014-08-01 17:26:32 -0300

    Improving function to detect browser version and replacing double quotes with single quotes.

--- source/assets/javascripts/locastyle/_browser-unsupported-bar.js
+++ source/assets/javascripts/locastyle/_browser-unsupported-bar.js
@@ -10,54 +10,34 @@ locastyle.browserUnsupportedBar = (function() {
 
   function browserDetect() {
     var userAgent = navigator.userAgent.toLowerCase();
+    var name = (userAgent.match(/(firefox|msie|chrome|version|rv)/))[1];
+    var version = parseInt((userAgent.match(/.+(?:firefox|msie|chrome|version|rv)[\/: ]([\d.]+)/) || [0, 0])[1].split('.')[0] );
 
-    var browserName = function() {
-      var name = (userAgent.match(/(firefox|msie|chrome|version)/))[1];
-      return name;
+    var minBrowserVersions = {
+      'chrome' : 34,
+      'firefox': 29,
+      'version':  5, // Safari
+      'msie'   :  8, // Internet Explorer
+      // 'rv'  : 11  // Internet Explorer 11+
+    };
+
+    if (!$.cookie('hideBrowserUnsupportedAlert')) {
+      if (version <=  minBrowserVersions[name]) {
+        openUsupportedBrowserAlert();
+      }
     }
 
-    var browserVersion = function() {
-      var version = (userAgent.match(/.+(?:firefox|msie|chrome|version)[\/: ]([\d.]+)/) || [0, 0])[1];
-      if(version === 0) {
-        // IE
-        version = (userAgent.match(/.+rv:([\d.]+)/) || 0)[1];
-      }
-      return version;
-    };
-
-    if (!$.cookie("hideBrowserUnsupportedAlert")) {
-      // Expect browser should be chrome and version minor 34
-      if (browserName() === 'chrome' && browserVersion() < '34'){
-        openUsupportedBrowserAlert();
-      };
-
-      // Expect browser should be firefox and version minor 29
-      if (browserName() === 'firefox' && browserVersion() < '29') {
-        openUsupportedBrowserAlert();
-      };
-
-      // Expect browser should be safari and version minor 5
-      if (browserName() === 'version' && browserVersion() < '5') {
-        openUsupportedBrowserAlert();
-      };
-
-      // Expect browser should be Internet Explorer and version minor 5
-      if (browserName() === 'msie' && browserVersion() < '9') {
-        openUsupportedBrowserAlert();
-      };
-
-    };
   }
 
   function openUsupportedBrowserAlert() {
-    $("html").addClass("ls-browser-unsupported");
-    $("body").prepend('<div class="ls-alert-warning ls-alert-blocker ls-dismissable"><span class="ls-dismiss" data-ls-module="dismiss">&times</span>Atualize seu navegador para visualizar corretamente este painel! Baixe a versão mais recente: Firefox, Chrome, Internet Explorer ou Safari</div>');
+    $('html').addClass('ls-browser-unsupported');
+    $('body').prepend('<div class="ls-alert-warning ls-alert-blocker ls-dismissable"><span class="ls-dismiss" data-ls-module="dismiss">&times</span>Atualize seu navegador para visualizar corretamente este painel! Baixe a versão mais recente: Firefox, Chrome, Internet Explorer ou Safari</div>');
   }
 
   function hideBrowserUnsupportedAlert() {
-    $(".ls-dismiss[data-ls-module=dismiss]").on("click", function() {
-      $("html").removeClass("ls-browser-unsupported");
-      $.cookie("hideBrowserUnsupportedAlert", true, { expires: 1 });
+    $('.ls-dismiss[data-ls-module=dismiss]').on('click', function() {
+      $('html').removeClass('ls-browser-unsupported');
+      $.cookie('hideBrowserUnsupportedAlert', true, { expires: 1 });
     });
   }
 
