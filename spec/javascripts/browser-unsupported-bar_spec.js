a373b9b0c6925e46d4a2d281ab1851ec4747a0d2
Antonio Junior <junyor@hotmail.co.uk>
2014-08-04 18:44:41 -0300

    Create test when click to close alert browser unsupported.

--- spec/javascripts/browser-unsupported-bar_spec.js
+++ spec/javascripts/browser-unsupported-bar_spec.js
@@ -22,4 +22,16 @@ describe("Alert unsupported browser: ", function(){
     })
   });
 
+  describe("When click to close", function() {
+    it("should remove .ls-browser-unsupported class on <html>", function() {
+      $('body').prepend('<div class="ls-alert-warning ls-alert-blocker ls-dismissable"><span class="ls-dismiss" data-ls-module="dismiss">&times</span>Atualize seu navegador para visualizar corretamente este painel! Baixe a versão mais recente: <a href="//www.mozilla.org/en-US/firefox" target="_blank">Firefox</a>, <a href="//www.google.com/intl/en-BR/chrome/browser/" target="_blank">Chrome</a>, <a href="//windows.microsoft.com/en-us/internet-explorer/download-ie" target="_blank">Internet Explorer</a> ou <a href="//www.apple.com/safari/" target="_blank">Safari</a></div>');
+
+      locastyle.browserUnsupportedBar.init();
+      $('.ls-alert-blocker .ls-dismiss[data-ls-module=dismiss]').trigger('click');
+
+      expect($('html').hasClass('ls-browser-unsupported')).toEqual(false);
+      $(".ls-alert-blocker").remove();
+    })
+  })
+
 });
