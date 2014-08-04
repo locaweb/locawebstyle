8fd8f95d2103f5c484bfa2879f73579359ee8775
Antonio Junior <junyor@hotmail.co.uk>
2014-08-04 18:44:56 -0300

    Small adjusts.

--- spec/javascripts/browser-unsupported-bar_spec.js
+++ spec/javascripts/browser-unsupported-bar_spec.js
@@ -1,16 +1,16 @@
-describe("Alert unsupported browser: ", function(){
-  beforeEach(function(){
+describe("Alert unsupported browser: ", function() {
+  beforeEach(function() {
     loadFixtures('browser-unsupported-bar_fixture.html');
   });
 
-  describe("When userAgent is MSIE", function(){
-    it("should add .ls-browser-unsupported class on <html>", function(){
+  describe("When userAgent is MSIE", function() {
+    it("should add .ls-browser-unsupported class on <html>", function() {
       $.cookie = function (arg) {
         return false;
       };
 
       var __originalLowerCase = String.prototype.toLowerCase;
-      String.prototype.toLowerCase = function () {
+      String.prototype.toLowerCase = function() {
         return "mozilla/4.0 (compatible; msie 8.0; windows nt 6.0; trident/4.0)";
       }
 
