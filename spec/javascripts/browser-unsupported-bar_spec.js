15bf041368728814777d1d8adca270d4d342a07d
Renato Nitta <renatocn@gmail.com>
2014-08-04 16:23:01 -0300

    fixing navigator.userAgent back and removing the html

--- spec/javascripts/browser-unsupported-bar_spec.js
+++ spec/javascripts/browser-unsupported-bar_spec.js
@@ -5,12 +5,17 @@ describe("Alert unsupported browser: ", function(){
 
   describe("When userAgent is MSIE", function(){
     it("should add .ls-browser-unsupported class on <html>", function(){
+      var __originalUserAgent = navigator.userAgent;
       navigator.__defineGetter__('userAgent', function () {
         return 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)';
       });
       locastyle.browserUnsupportedBar.init();
       expect($('html').hasClass('ls-browser-unsupported')).toEqual(true);
-      $('.ls-alert-blocker').remove();
+
+      $(".ls-alert-blocker").remove();
+      navigator.__defineGetter__('userAgent', function () {
+        return __originalUserAgent;
+      });
     })
   });
 
