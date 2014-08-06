5282a920fef9bf7850117b2723b4fdd9710093be
letanure <letanure@gmail.com>
2014-08-06 16:54:29 -0300

    fix datepicker

--- source/assets/javascripts/locastyle/_datepicker.js
+++ source/assets/javascripts/locastyle/_datepicker.js
@@ -3,25 +3,39 @@ var locastyle = locastyle || {};
 locastyle.datepicker = (function() {
   'use strict';
 
+  var config = {
+    selector: '.datepicker'
+  };
+
   function init() {
-    var $datepicker = $('#datepicker').pikaday({
-        firstDay: 1,
-        minDate: new Date('2000-01-01'),
-        maxDate: new Date('2020-12-31'),
-        yearRange: [2000,2020],
-        i18n: {
-          previousMonth : 'Anterior',
-          nextMonth     : 'Próximo',
-          months        : ['Janeiro','Fevereiro','Março','Abril','Maio','Junnho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
-          weekdays      : ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
-          weekdaysShort : ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb']
-        }
+    $(config.selector).each(function(){
+      create($(this));
     });
-    $datepicker.pikaday('show').pikaday('nextMonth');
+  }
+
+  function create($elem){
+    var $datepicker = $elem.pikaday({
+      firstDay: 1,
+      minDate: new Date('2000-01-01'),
+      maxDate: new Date('2020-12-31'),
+      yearRange: [2000,2020],
+      i18n: {
+        previousMonth : 'Anterior',
+        nextMonth     : 'Próximo',
+        months        : ['Janeiro','Fevereiro','Março','Abril','Maio','Junnho','Julho','Agosto','Setembro','Outubro','Novembro','Dezembro'],
+        weekdays      : ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
+        weekdaysShort : ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb']
+      }
+    });
+  }
+
+  function newDatepicker(selector){
+   create($(selector)); 
   }
 
   return {
-    init: init
+    init: init,
+    newDatepicker: newDatepicker
   };
 
 }());
