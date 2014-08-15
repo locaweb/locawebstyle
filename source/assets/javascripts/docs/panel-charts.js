bbf28382b6ce676cf616b3c4736ee8bd16f884c7
Antonio Junior <junyor@hotmail.co.uk>
2014-08-15 14:00:15 -0300

    Replacing double quotes to single quotes. #976

--- source/assets/javascripts/docs/panel-charts.js
+++ source/assets/javascripts/docs/panel-charts.js
@@ -1,4 +1,6 @@
 $(function () {
+
+  // panel 1
   $('#panel-charts').highcharts({
     chart: {
       type: 'column'
@@ -7,12 +9,7 @@ $(function () {
       text: 'Agosto/2014'
     },
     xAxis: {
-      categories: [
-        'Joana',
-        'Marcela',
-        'Patricia',
-        'Diego'
-      ]
+      categories: ['Joana', 'Marcela', 'Patricia', 'Diego']
     },
     yAxis: {
       min: 0,
@@ -20,7 +17,7 @@ $(function () {
         text: 'Envios'
       }
     },
-    colors: ["#bdaa38", "#aa4643 ","#89a54e"],
+    colors: ['#bdaa38', '#aa4643 ','#89a54e'],
     series: [{
       name: 'Total de envios contratados',
       data: [25, 50, 20, 3]
@@ -37,12 +34,7 @@ $(function () {
       text: 'Agosto/2014'
     },
     xAxis: {
-      categories: [
-        '29/04 a 28/05',
-        '29/06 a 28/07',
-        '29/08 a 28/09',
-        '29/10 a 28/11'
-      ]
+      categories: ['29/04 a 28/05', '29/06 a 28/07', '29/08 a 28/09', '29/10 a 28/11']
     },
     yAxis: {
       min: 0,
@@ -50,7 +42,7 @@ $(function () {
         text: 'Envios'
       }
     },
-    colors: ["#bdaa38", "#aa4643 ","#89a54e"],
+    colors: ['#bdaa38', '#aa4643 ','#89a54e'],
     series: [{
       name: 'Mensagens enviadas',
       data: [30, 15, 30, 30],
@@ -65,20 +57,16 @@ $(function () {
     }]
   });
 
+  // Panel 2
   $('#panel-charts-2').highcharts({
     chart: {
       type: 'column'
     },
     title: {
-      text: 'Agosto/2014'
+      text: 'Relatório'
     },
     xAxis: {
-      categories: [
-        '00h',
-        '01h',
-        '02h',
-        '03h'
-      ]
+      categories: ['00h', '01h', '02h', '03h']
     },
     yAxis: {
       min: 0,
@@ -86,6 +74,7 @@ $(function () {
         text: 'Quantidade'
       }
     },
+    colors: ['#25b89a', '#aa4643 ','#89a54e'],
     series: [{
       name: 'envios',
       data: [55, 50, 20, 3]
