1c88b933564a0556296ffee05dc1bc686e845e1a
Deivid Marques <deividmarques@gmail.com>
2014-10-20 15:40:22 -0200

    adding wrap when select has class ls-custom-select

--- source/assets/javascripts/locastyle/_custom-fields.js
+++ source/assets/javascripts/locastyle/_custom-fields.js
@@ -4,41 +4,16 @@ locastyle.customFields = (function() {
   'use strict';
 
   function init() {
-    findFields();
-  }
+    customSelect();
+  };
 
-  function findFields(){
-    $('[data-ls-module="customFields"]').find(':input').not('.ls-input-original').each(function(index, field){
-      var fieldType = $(field)[0].type;
-      var classParent = $(field).eq(0).attr('class');
-      classParent = classParent !== undefined ? classParent : '';
-
-      $(field).wrap( '<div class="ls-custom-' + fieldType + ' ' + classParent + '" />' );
-      fieldSpan($(field));
+  function customSelect(){
+    $('.ls-custom-select').wrap(function(){
+      var selectClasses = $(this).attr('class');
+      $(this).removeAttr('class').addClass('ls-select');
+      return '<div class="'+selectClasses+'"></div>';
     });
-  }
-
-  function fieldSpan($field){
-    var selectText = $field.children('option:selected').eq(0).text();
-    $('<span class="ls-field-custom-'+ $field[0].type + '">' + selectText + '</span>').insertAfter($field[0]);
-    bindCustomSelect($field);
-  }
-
-  function siblingsField($field){
-    return $field.siblings('.ls-field-custom-'+ $field[0].type);
-  }
-
-  function bindCustomSelect($field){
-    unbind($field);
-    var $siblingsField = siblingsField($field);
-    $field.on('change.ls', function(){
-      $siblingsField.text($field.find('option:selected').text());
-    });
-  }
-
-  function unbind ($field) {
-    $($field).off('change.ls');
-  }
+  };
 
   return {
     init: init
