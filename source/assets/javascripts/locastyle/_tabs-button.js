f154e4042e392a6b6db350458cb87fc50c7cc1c2
Deivid Marques <deividmarques@gmail.com>
2015-01-15 17:14:26 -0200

    removing comments

--- source/assets/javascripts/locastyle/_tabs-button.js
+++ source/assets/javascripts/locastyle/_tabs-button.js
@@ -13,7 +13,6 @@ locastyle.button = (function() {
     $("[data-ls-module=button]").off("click.button");
   }
 
-  // adiciona o bind de click no modulo e chama os métodos necessários
   function bindClickOnTriggers() {
     $("[data-ls-module=button]").on("click.button", function(evt) {
       evt.preventDefault();
@@ -25,14 +24,12 @@ locastyle.button = (function() {
     });
   }
 
-  // ativa o elemento de acordo com os argumentos recebidos
   function activateElement(el, $target) {
     $(el).parents("li").addClass("ls-active");
     $target.addClass("ls-active");
     $(el).attr('aria-selected' , true);
   }
 
-  // desativa o elemento de acordo com os argumentos recebidos
   function deactivateElement(el, $target, $buttons) {
     $(el).parents("li").siblings().removeClass("ls-active");
     $target.siblings().removeClass("ls-active");
