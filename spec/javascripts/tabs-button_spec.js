0581eab0f1ceb7ac1275526db9e624c78010728c
Deivid Marques <deividmarques@gmail.com>
2015-01-15 15:23:11 -0200

    end jasmine tests

--- spec/javascripts/tabs-button_spec.js
+++ spec/javascripts/tabs-button_spec.js
@@ -18,94 +18,80 @@ describe("Tabs: ", function() {
         expect($("#tab1").hasClass("ls-active")).toEqual(true);
       });
 
-      // it("should activate the related target tab", function() {
-      //   $("#tab-trigger-2").trigger("click");
-      //   expect($("#laps").hasClass("ls-active")).toEqual(true);
-      // });
+      it("should deactivate the siblings tabs of related target tab", function() {
+        $("#tab-button-2").trigger("click");
+        expect($("#tab2").hasClass("ls-active")).toEqual(true);
+      });
 
-      // it("should prevent default event", function() {
-      //   var spyEvent = spyOnEvent('#tab-trigger-2', 'click');
-      //   $("#tab-trigger-2").trigger("click");
-      //   expect(spyEvent).toHaveBeenPrevented();
-      // });
+      it("should prevent default event", function() {
+        var spyEvent = spyOnEvent('#tab-button-1', 'click');
+        $("#tab-button-1").trigger("click");
+        expect(spyEvent).toHaveBeenPrevented();
+      });
 
-      // it("should activate the parent li", function() {
-      //   var $parentLi = $("#tab-trigger-2").parents("li");
-      //   $("#tab-trigger-2").trigger("click");
-      //   expect($($parentLi).hasClass("ls-active")).toEqual(true);
-      // });
+      it("should activate the parent li", function() {
+        var $parentLi = $("#tab-button-1").parents("li");
+        $("#tab-button-1").trigger("click");
+        expect($($parentLi).hasClass("ls-active")).toEqual(true);
+      });
 
-      // it("should deactivate the siblings of parent li", function() {
-      //   var $siblingOfParentLi = $("#tab-trigger-1").parents("li");
-      //   $("#tab-trigger-2").trigger("click");
-      //   expect($siblingOfParentLi.hasClass("ls-active")).toEqual(false);
-      // });
+      it("should deactivate the siblings of parent li", function() {
+        var $siblingOfParentLi = $("#tab-button-1").parents("li");
+        $("#tab-button-2").trigger("click");
+        expect($siblingOfParentLi.hasClass("ls-active")).toEqual(false);
+      });
 
-      // describe('and tabs is in dropdown mode', function () {
-      //   it("should update the trigger value with the current active tab value", function () {
-      //     $("#my-tabs-in-dropdown-shape #tab-trigger-6").trigger("click");
-      //     expect($("#my-tabs-in-dropdown-shape .ls-dropdown-tabs > a").text()).toEqual("Laps");
-      //   });
-      // });
     });
-
-    // describe("when click on element with data-ls-module without a href", function() {
-    //   it("should use a data-target to active tab", function() {
-    //     $('#myButtons').trigger("click");
-    //     expect($('#myButtons').hasClass("ls-active")).toEqual(true);
-    //   });
-    // });
-
 
   });
 
-  // describe("Unbind:", function() {
-  //   describe("when unbind is called in module", function() {
-  //     it("should unbind events handled by module", function() {
-  //       locastyle.tabs.unbind();
-  //       $('#tab-trigger-2').trigger("click");
-  //       expect($('#laps').hasClass("ls-active")).toEqual(false);
-  //     });
+  describe("Unbind:", function() {
+    describe("when unbind is called in module", function() {
+      it("should unbind events handled by module", function() {
+        locastyle.button.unbind();
+        $('#tab-button-2').trigger("click");
+        expect($('#tab2').hasClass("ls-active")).toEqual(false);
+      });
 
-  //     it("should NOT unbind common events handled by other code", function() {
-  //       window.test = {
-  //         method: function(){
-  //           //my fake function
-  //         }
-  //       };
-  //       var spy = spyOn(window.test, "method");
-  //       $('#tab-trigger-2').on("click", function () {
-  //         window.test.method();
-  //       });
-  //       locastyle.tabs.unbind();
-  //       $('#tab-trigger-2').trigger("click");
-  //       expect(window.test.method).toHaveBeenCalled();
-  //     });
-  //   });
-  // });
+      it("should NOT unbind common events handled by other code", function() {
+        window.test = {
+          method: function(){
+            //my fake function
+          }
+        };
+        var spy = spyOn(window.test, "method");
+        $('#tab-button-2').on("click", function () {
+          window.test.method();
+        });
+        locastyle.tabs.unbind();
+        $('#tab-button-2').trigger("click");
+        expect(window.test.method).toHaveBeenCalled();
+      });
+    });
+  });
 
-  // describe("Tabs with wai-aria", function() {
-  //   it(".ls-tabs-nav should has attribute role with value tablist ",function(){
-  //     expect($('.ls-tabs-nav').attr('role')).toEqual('tablist');
-  //   });
+  describe("Tabs button with wai-aria", function() {
+    it(".ls-tabs-btn-nav should has attribute role with value tablist ",function(){
+      expect($('.ls-tabs-btn-nav').attr('role')).toEqual('tablist');
+    });
 
-  //   it("Link tabs should has attribute role with value tab ",function(){
-  //     expect($('.ls-tabs-nav a').attr('role')).toEqual('tab');
-  //   });
+    it("Link tabs should has attribute role with value tab ",function(){
+      expect($('.ls-tabs-btn-nav .ls-btn').attr('role')).toEqual('tab');
+    });
 
-  //   it("Active link tabs should has attribute aria-selected is value true ",function(){
-  //     expect($('.ls-tabs-nav li.ls-active a').attr('aria-selected')).toEqual('true');
-  //   });
+    it("Active link tabs should has attribute aria-selected is value true ",function(){
+      expect($('.ls-tabs-btn-nav li.ls-active .ls-btn').attr('aria-selected')).toEqual('true');
+    });
 
-  //   it(".ls-tab-content has attribute role with value tabpanel ",function(){
-  //     expect($('.ls-tab-content').attr('role')).toEqual('tabpanel');
-  //   });
+    it(".ls-tab-content has attribute role with value tabpanel ",function(){
+      expect($('.ls-tabs-btn .ls-tab-content').attr('role')).toEqual('tabpanel');
+    });
 
-  //   it("When click tab should has attribute aria-selected with value true ",function(){
-  //     $('.ls-tabs-nav a').trigger('click');
-  //     expect($('.ls-tabs-nav li.ls-active a').attr('aria-selected')).toEqual('true');
-  //   });
+    it("When click tab should has attribute aria-selected with value true ",function(){
+      $('.ls-tabs-btn-nav .ls-btn').trigger('click');
+      expect($('.ls-tabs-btn-nav li.ls-active .ls-btn').attr('aria-selected')).toEqual('true');
+    });
 
-  // });
+  });
 
 });
