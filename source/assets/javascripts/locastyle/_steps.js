7be096613aeb8be1c7888bc886c57246a7d64d48
Diego Eis <diegoeis@gmail.com>
2015-01-23 15:13:13 -0200

    '' is defined but never used.

--- source/assets/javascripts/locastyle/_steps.js
+++ source/assets/javascripts/locastyle/_steps.js
@@ -42,7 +42,6 @@ locastyle.steps = (function() {
     var $steps   = $(config.selectors.nav);
     var offset    = $steps.offset();
     var $heightNav  = $(config.selectors.nav).height();
-    var $areaStep = parseInt(elemVisible + ($heightNav * 2));
 
     $(window).scroll(function() {
      if ($(window).scrollTop() > offset.top ){
