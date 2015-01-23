49a751a9509708ee25897ef78cba600ad1c656c0
Diego Eis <diegoeis@gmail.com>
2015-01-23 15:26:24 -0200

    missing semicolon

--- source/assets/javascripts/locastyle/_dropdown.js
+++ source/assets/javascripts/locastyle/_dropdown.js
@@ -10,7 +10,7 @@ locastyle.dropdown = (function() {
     button: '[class*="ls-btn"]',
     firstLink: '[data-ls-module="dropdown"] > [class*="ls-btn"]:first-child, .ls-dropdown.ls-user-account > a:first-child',
     nav: '.ls-dropdown-nav'
-  }
+  };
 
   function init() {
     unbind();
