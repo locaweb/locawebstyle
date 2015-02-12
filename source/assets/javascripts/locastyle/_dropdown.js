c00cefa03387dcd81bc07f5237c39472a55288e9
Pedro Bartulihe <pedro.bartulihe@gmail.com>
2015-02-12 10:22:37 -0200

    Prototyping Dropdown

--- source/assets/javascripts/locastyle/_dropdown.js
+++ source/assets/javascripts/locastyle/_dropdown.js
@@ -4,77 +4,117 @@ locastyle.dropdown = (function() {
   'use strict';
 
   var config = {
-    area: 'body',
-    dropdown: '.ls-dropdown',
-    module: '[data-ls-module="dropdown"]',
-    button: '[class*="ls-btn"]',
-    firstLink: '[data-ls-module="dropdown"] > [class*="ls-btn"]:first-child, .ls-dropdown.ls-user-account > a:first-child',
-    nav: '.ls-dropdown-nav'
-  }
+        selectors : {
+          area: 'body',    
+          nav: '.ls-dropdown-nav',
+          firstLink: ' > [class*="ls-btn"]:first-child,  > a:first-child',
+        },        
+        classes : {
+          disabled : 'ls-disabled',
+          open : 'ls-active',
+          alignToRight : 'ls-pos-right'
+        }
+      }
+  ;
+  var _list;
 
   function init() {
-    unbind();
-    bindClickOnTriggers();
-    bindClickOutsideTriggers();
-    ariaDropdown(config.dropdown);
+    _list = [];
+    $('[data-ls-module="dropdown"], .ls-dropdown.ls-user-account').each(function() {
+      _list.push(new Dropdown($(this)));
+    });
   }
-
   function unbind() {
-    $(config.firstLink).off("click.ls");
-    $(config.area).off("click.ls");
+    for (var i = 0; i < _list.length; i++) {
+      _list[i].removeEvents();
+    };
   }
-
-  function bindClickOnTriggers() {
-    $(config.firstLink).on("click.ls", function(evt) {
-      evt.preventDefault();
-      var $target = $($(this).parents(config.module));
-      locastyle.dropdown.toggleDropdown($target);
-      ariaDropdown($target);
-      locastyle.dropdown.closeDropdown($target);
-      setPositionVisible($target);
-      evt.stopPropagation();
-    });
-  }
-
-  function bindClickOutsideTriggers() {
-    $(config.area).on("click.ls", function(){
-      locastyle.dropdown.closeDropdown();
-    });
-  }
-
   function toggleDropdown($target) {
-    if(!$target.find('config.button:eq(0)').hasClass('ls-disabled')){
-      $target.toggleClass("ls-active");
-      $(config.button).attr({ 'aria-expanded' : 'false' });
-      $(config.nav).attr({ 'aria-hidden' : 'true' });
+    $($target).find(config.selectors.firstLink).trigger('click');
+  }
+  function closeDropdown() {
+    for (var i = 0; i < _list.length; i++) {
+      _list[i].close();
+    };
+  }
+
+  // 
+  // Dropdown
+  // 
+  function Dropdown(element) {
+    this.$el        = $(element); 
+    this.$firstLink = this.$el.find(config.selectors.firstLink);
+    this.$nav       = this.$el.find(config.selectors.nav);
+    this.$body      = $('body');
+
+    this.addEvents();   
+
+    this.$nav.find('a').attr({ role : 'option' });
+    this.$firstLink.attr({ role : 'combobox' });
+
+    if(this.isOpen()) {        
+      this.open();
+    } else {
+      this.close();
+    }
+    
+  }  
+  Dropdown.prototype = {
+    addEvents : function() {
+      this.$body.on('click.ls.dropdown', this.onDocumentClick.bind(this));
+      this.$firstLink.on('click.ls.dropdown', this.onFirstLinkClick.bind(this));      
+    },
+
+    removeEvents : function() {
+      this.$body.off('click.ls.dropdown');
+      this.$firstLink.off('click.ls.dropdown');
+    },
+    isOpen : function() {
+      return this.$el.hasClass(config.classes.open);
+    },
+    isDisable : function() {
+      return this.$firstLink.hasClass(config.classes.disabled);
+    },
+    open : function() {      
+      if(!this.isDisable()) {
+        this.$el.addClass(config.classes.open);
+
+        if(this.$body.get(0).scrollWidth > this.$body.width()) {
+          this.$el.addClass(config.classes.alignToRight);
+        }
+
+        this.$firstLink.attr({ 'aria-expanded' : 'true' });
+        this.$nav.attr({ 'aria-hidden' : 'false' });
+      }      
+    },
+
+    close : function() {
+      console.log("close");
+      this.$el.removeClass(config.classes.open).removeClass(config.classes.alignToRight);
+
+      this.$firstLink.attr({ 'aria-expanded' : 'false' });
+      this.$nav.attr({ 'aria-hidden' : 'true' });
+    },
+    toggle : function() {
+      if(this.isOpen()) {        
+        this.close();
+      } else {
+        this.open();
+      }
+        
       locastyle.topbarCurtain.hideCurtains();
-    }
-  }
+    },
 
-  function closeDropdown(el) {
-    $(config.module).not(el).removeClass("ls-active");
-  }
-
-  function setPositionVisible($target){
-    var $main = $(config.area);
-    if($main.get(0).scrollWidth > $main.width()){
-      $($target).addClass('ls-pos-right');
+    onDocumentClick : function(event) {      
+      if ( !$(event.target).is(this.$firstLink) && this.isOpen() ) {        
+        this.close();
+      }
+    },
+    onFirstLinkClick : function(event) {
+      event.preventDefault();
+      this.toggle();
     }
-  }
-
-  function ariaDropdown(el) {
-    $(config.nav, el).find('a').attr({ role : 'option' });
-    $(config.button, el).attr({ role : 'combobox' });
-
-    if($(el).hasClass('ls-active')){
-      $(config.button, el).attr({ 'aria-expanded' : 'true' });
-      $(config.nav).attr({ 'aria-hidden' : 'false' });
-    }
-    else{
-      $(config.button, el).attr({ 'aria-expanded' : 'false' });
-      $(config.nav, el).attr({ 'aria-hidden' : 'true' });
-    }
-  }
+  };
 
   return {
     init: init,
