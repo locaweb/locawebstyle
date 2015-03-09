97d315224de5b983423e949a786da817c0a2fe38
Pedro Bartulihe <pedro.bartulihe@gmail.com>
2015-03-09 12:13:12 -0300

    fix jshint

--- source/assets/javascripts/locastyle/_char-counter.js
+++ source/assets/javascripts/locastyle/_char-counter.js
@@ -13,27 +13,27 @@ locastyle.charCounter = (function() {
 
   function countText() {
     $fields.each(function(index, field) {
-      var $this     = $(field);      
-      if ($this.parent().find('.ls-number-counter-'+index).length == 0) {
-        $this.attr('data-index', index).after('<p class="ls-help-inline"><small><strong class="ls-char-count ls-number-counter-'+index+'">0</strong> caracteres restantes</small></p>');  
+      var $this     = $(field);
+      if ($this.parent().find('.ls-number-counter-' + index).length === 0) {
+        $this.attr('data-index', index).after('<p class="ls-help-inline"><small><strong class="ls-char-count ls-number-counter-' + index + '">0</strong> caracteres restantes</small></p>');
       }
       updateCounter($this);
     });
   }
 
   function bind() {
-    $fields.off("keyup.ls.charCounter").on("keyup.ls.charCounter", function(event) {              
+    $fields.off("keyup.ls.charCounter").on("keyup.ls.charCounter", function() {
       updateCounter($(this));
     });
   }
 
   function updateCounter(field) {
     var $this = $(field),
-        text  = $this.val(),        
+        text  = $this.val(),
         countEnters = text.match(/(\r\n|\n|\r)/g),
-        count = $this.attr('maxlength') - text.length - (countEnters == null ? 0 : countEnters.length)
+        count = $this.attr('maxlength') - text.length - (countEnters === null ? 0 : countEnters.length)
     ;
-    $('.ls-number-counter-'+$this.attr('data-index')).text(count);
+    $('.ls-number-counter-' + $this.attr('data-index')).text(count);
   }
 
   return {
