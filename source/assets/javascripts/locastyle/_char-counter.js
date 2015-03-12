b83c0fc402af5d5a68d49d8238df8c8941bd45d6
Pedro Bartulihe <pedro.bartulihe@gmail.com>
2015-03-12 13:29:56 -0300

    Fix char count for textarea

--- source/assets/javascripts/locastyle/_char-counter.js
+++ source/assets/javascripts/locastyle/_char-counter.js
@@ -13,7 +13,7 @@ locastyle.charCounter = (function() {
 
   function countText() {
     $fields.each(function(index, field) {
-      var $this     = $(field);
+      var $this = $(field);
       if ($this.parent().find('.ls-number-counter-' + index).length === 0) {
         $this.attr('data-index', index).after('<p class="ls-help-inline"><small><strong class="ls-char-count ls-number-counter-' + index + '">0</strong> caracteres restantes</small></p>');
       }
@@ -22,7 +22,7 @@ locastyle.charCounter = (function() {
   }
 
   function bind() {
-    $fields.off("keyup.ls.charCounter").on("keyup.ls.charCounter", function() {
+    $fields.off("input.ls.charCounter propertychange.ls.charCounter").on("input.ls.charCounter propertychange.ls.charCounter", function() {
       updateCounter($(this));
     });
   }
@@ -30,10 +30,14 @@ locastyle.charCounter = (function() {
   function updateCounter(field) {
     var $this = $(field),
         text  = $this.val(),
-        countEnters = text.match(/(\r\n|\n|\r)/g),
-        count = $this.attr('maxlength') - text.length - (countEnters === null ? 0 : countEnters.length)
-    ;
-    $('.ls-number-counter-' + $this.attr('data-index')).text(count);
+        max   = $this.attr('maxlength'),
+        countEnters = (text.match(/\n/g) || []).length;
+
+    if (text.length + countEnters > max) {
+        $this.val( text.substring(0, max - countEnters) );
+    }
+
+    $('.ls-number-counter-' + $this.attr('data-index')).text(max - (text.length + countEnters));
   }
 
   return {
