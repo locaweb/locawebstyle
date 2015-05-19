7d9e6218b5219f292899a7150c4fca5c8eb1ee63
Palloi Hofmann <leonard.hofmann@locaweb.com.br>
2015-05-19 10:09:24 -0300

    change variable name

--- source/assets/javascripts/locastyle/_breakpoint-check.js
+++ source/assets/javascripts/locastyle/_breakpoint-check.js
@@ -9,13 +9,16 @@ locastyle.breakpoints = (function() {
   var config = {
     sm: '768',
     md: '992',
-    lg: '1200'
+    lg: '1200',
+    html: null
   };
 
   function init(userConfig) {
+    config.html = $('html');
+
     breakpointWindowWidth(userConfig);
-    changeClassBreakpoint();
     breakpointScreenWidth();
+    changeClassBreakpoint();
   }
 
   //
@@ -23,6 +26,7 @@ locastyle.breakpoints = (function() {
   //
   function breakpointWindowWidth(userConfig) {
     var documentWidth;
+
     if (userConfig){
       documentWidth = userConfig.documentWidth;
     } else {
@@ -31,59 +35,68 @@ locastyle.breakpoints = (function() {
 
     // Se for menor que 768 - xs
     if (documentWidth < config.sm) {
-      $('html').addClass('ls-window-xs');
+      config.html.addClass('ls-window-xs');
       locastyle.breakpointClass = "ls-window-xs";
     }
 
     // Se for maior ou igual a 768 e menor que 992 - sm
     else if (documentWidth >= config.sm && documentWidth < config.md) {
-      $('html').addClass('ls-window-sm').removeClass('ls-sidebar-visible ls-notifications-visible ');
+      config.html.addClass('ls-window-sm').removeClass('ls-sidebar-visible ls-notifications-visible ');
       locastyle.breakpointClass = "ls-window-sm";
     }
 
     // Se for maior ou igual a 992 e menor que 1200 - md
     else if (documentWidth >= config.md && documentWidth < config.lg) {
-      $('html').addClass('ls-window-md').removeClass('ls-sidebar-visible ls-notifications-visible ');
+      config.html.addClass('ls-window-md').removeClass('ls-sidebar-visible ls-notifications-visible ');
       locastyle.breakpointClass = "ls-window-md";
     }
 
     // Se for maior ou igual a 1200 - lg
     else {
-      $('html').addClass('ls-window-lg').removeClass('ls-sidebar-visible ls-notifications-visible ');
+      config.html.addClass('ls-window-lg').removeClass('ls-sidebar-visible ls-notifications-visible ');
       locastyle.breakpointClass = "ls-window-lg";
     }
   }
 
-  function breakpointScreenWidth() {
-    var documentWidth = screen.width;
+  //
+  // Alterando a classe na tag html de acordo com o screen.
+  //
+  function breakpointScreenWidth(userConfig) {
+    var screenWidth;
+    
+    if (userConfig){
+      screenWidth = userConfig.documentWidth;
+    } else {
+      screenWidth = screen.width;
+    }
 
     // Se for menor que 768 - xs
-    if (documentWidth < config.sm) {
-      $('html').addClass('ls-screen-xs');
+    if (screenWidth < config.sm) {
+      config.html.addClass('ls-screen-xs');
       locastyle.breakpointScreenClass = "ls-screen-xs";
     }
 
     // Se for maior ou igual a 768 e menor que 992 - sm
-    else if (documentWidth >= config.sm && documentWidth < config.md) {
-      $('html').addClass('ls-screen-sm');
+    else if (screenWidth >= config.sm && screenWidth < config.md) {
+      config.html.addClass('ls-screen-sm');
       locastyle.breakpointScreenClass = "ls-screen-sm";
     }
 
     // Se for maior ou igual a 992 e menor que 1200 - md
-    else if (documentWidth >= config.md && documentWidth < config.lg) {
-      $('html').addClass('ls-screen-md');
+    else if (screenWidth >= config.md && screenWidth < config.lg) {
+      config.html.addClass('ls-screen-md');
       locastyle.breakpointScreenClass = "ls-screen-md";
     }
 
     // Se for maior ou igual a 1200 - lg
     else {
-      $('html').addClass('ls-screen-lg');
+      config.html.addClass('ls-screen-lg');
       locastyle.breakpointScreenClass = "ls-screen-lg";
     }
   }
 
   //
-  // Alterando a classe na tag html quando é redimensionamos a janela.
+  // Alterando a classe na tag html quando redimensionamos a janela.
   //
   function changeClassBreakpoint() {
 
@@ -94,9 +107,9 @@ locastyle.breakpoints = (function() {
 
       changeClass = setTimeout(function() {
 
-        var breakpointActive = $('html').attr('class').replace(/(^|\s)(ls-window-\S+)|(ls-screen-\S+)/g, '');
+        var breakpointActive = config.html.attr('class').replace(/(^|\s)(ls-window-\S+)|(ls-screen-\S+)/g, '');
 
-        $('html').attr('class', $.trim(breakpointActive));
+        config.html.attr('class', $.trim(breakpointActive));
 
         breakpointWindowWidth();
         breakpointScreenWidth();
