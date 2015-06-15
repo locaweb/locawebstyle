71403ad5a63493ff82b83c80b32c8b676869bfc4
Deivid Marques <deividmarques@gmail.com>
2015-06-15 19:58:40 -0300

    creating jasmine tests

--- spec/javascripts/modal_spec.js
+++ spec/javascripts/modal_spec.js
@@ -74,6 +74,26 @@ describe("Modal: ", function() {
 
   });
 
+  describe('When click modal', function() {
+
+    beforeEach(function() {
+      $('#myModal').trigger("click");
+    });
+
+    describe('When modal is opened', function() {
+      it('tag body should have class .ls-overflow-hidden', function() {
+        expect($("body").hasClass('ls-overflow-hidden')).toEqual(true);
+      });
+    });
+
+    describe('When modal is closed', function() {
+      it('tag body not should have class .ls-overflow-hidden', function() {
+        locastyle.modal.close();
+        expect($("body").hasClass('ls-overflow-hidden')).toEqual(false);
+      });
+    });
+  });
+
   describe("Unbind:", function() {
     describe("when ESC keyup", function() {
       it("should unbind $document keyup event", function() {
