7e913de6cd8f34d9e59ea2c37f0e4bf345d794cb
Italo Moraes <italomoraes@outlook.com>
2015-07-14 08:50:07 -0300

    Switch button event.

--- source/assets/javascripts/locastyle/_switch-button.js
+++ source/assets/javascripts/locastyle/_switch-button.js
@@ -3,4 +3,32 @@ var locastyle = locastyle || {};
 locastyle.switchButton = (function() {
   'use strict';
 
+  var config = {
+    switchButton: '.ls-switch-btn',
+    openedClass: 'ls-switch-btn-active'
+  };
+
+  function init() {
+    bindEventOnChange();
+  }
+
+  function bindEventOnChange() {
+    $(config.switchButton).on('click.ls', function(e) {
+      eventHandler($(this));
+      e.stopPropagation();
+    });
+  }
+
+  function eventHandler(el) {
+    if (el.find('input[type=checkbox]').prop('checked')) {
+      el.trigger('switchButton:actived').addClass(config.openedClass);
+    } else {
+      el.trigger('switchButton:disabled').removeClass(config.openedClass);
+    }
+  }
+
+  return {
+    init: init
+  };
+
 })();
