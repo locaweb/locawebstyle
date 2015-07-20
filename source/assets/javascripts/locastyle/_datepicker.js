f19cadd831e42c59d7220d12e61b4422b0714a7b
Italo Moraes <italomoraes@outlook.com>
2015-07-20 17:13:14 -0300

    Datepicker creation with range.

--- source/assets/javascripts/locastyle/_datepicker.js
+++ source/assets/javascripts/locastyle/_datepicker.js
@@ -4,17 +4,9 @@ locastyle.datepicker = (function() {
   'use strict';
 
   var config = {
-    selector: '.datepicker'
-  };
-
-  function init() {
-    $(config.selector).each(function(){
-      create($(this));
-    });
-  }
-
-  function create($elem){
-   $elem.pikaday({
+    selector: '.datepicker',
+    rangeSelector: '[data-ls-daterange]',
+    pikaday: {
       firstDay: 1,
       minDate: new Date('2000-01-01'),
       maxDate: new Date('2020-12-31'),
@@ -27,7 +19,47 @@ locastyle.datepicker = (function() {
         weekdays      : ['Domingo','Segunda','Terça','Quarta','Quinta','Sexta','Sábado'],
         weekdaysShort : ['Dom','Seg','Ter','Qua','Qui','Sex','Sáb']
       }
+    }
+  };
+
+  function init() {
+    // Datepicker without range
+    $(config.selector).each(function(){
+      if (!$(this).hasClass('ls-daterange')) {
+        create($(this));
+      }
     });
+
+    // Datepicker with range
+    $(config.rangeSelector).each(function() {
+      createWithRange($(this));
+    });
+  }
+
+  function create(el){
+    el.pikaday(config.pikaday);
+  }
+
+  function createWithRange(el) {
+    var picker1 = null;
+    var picker2 = null;
+
+    var pickerStartObj = {
+      field: el[0],
+      onSelect: function() {
+        picker2.setMinDate(this.getDate());
+      }
+    };
+
+    var pickerEndObj = {
+      field: $(el.data('ls-daterange'))[0],
+      onSelect: function() {
+        picker1.setMaxDate(this.getDate());
+      }
+    };
+
+    picker1 = new Pikaday($.extend(pickerStartObj, config.pikaday));
+    picker2 = new Pikaday($.extend(pickerEndObj, config.pikaday));
   }
 
   function newDatepicker(selector){
