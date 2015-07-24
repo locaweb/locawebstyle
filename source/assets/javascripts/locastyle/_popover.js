994ca79fc9c8c628d94bf865827394c59e25274d
Flavio Muniz <flavio.junior@locaweb.com.br>
2015-07-24 15:27:31 -0300

    Init new popover.

--- source/assets/javascripts/locastyle/_popover.js
+++ source/assets/javascripts/locastyle/_popover.js
@@ -4,138 +4,46 @@ locastyle.popover = (function() {
   'use strict';
 
   var config = {
-    container    : 'body',
-    hoverEvent   : 'mouseenter.ls.popover',
-    idPopover    : '#ls-popover-',
-    module       : '[data-ls-module="popover"]',
-    placement    : 'top',
-    trigger      : 'click.ls.popover',
-    popoverClass : 'ls-popover',
-    activeClass  : 'ls-active',
-    uniqueId     : 0
-  };
+    module: '[data-ls-module="popover"]',
+    init: {
+      $container    : $(document),
+      idPopover    : '#ls-popover-',
+      trigger      : 'click.ls.popover',
+      popoverClass : 'ls-popover',
+      activeClass  : 'ls-active',
+      uniqueId     : 0
+    }
+  }
 
   function init() {
-    if(/sm|xs/.test(locastyle.breakpointClass)){
-      $(config.module).attr('data-ls-module', 'modal').removeAttr('data-trigger');
-      locastyle.modal.init();
-    } else{
-      $(config.module).each(function  (index, elem) {
-        createPopover($(elem));
-      });
-    }
-    updateBreakpoint();
+    checkExists();
   }
 
-  function updateBreakpoint() {
-    $(window).on("breakpoint-updated", function () {
-      destroy();
-      init();
+  function checkExists() {
+    $(config.module).each(function(index, el) {
+
+      if(!$(config.init.idPopover+index).length) {
+        buildPopover(index, el);
+      }
+
     });
   }
 
-  function createPopover($elem) {
-    var elementData = $elem.data(),
-        width  = $elem.outerWidth(),
-        height = $elem.outerHeight();
-    $elem.data('uniqueId', config.uniqueId);
-    elementData.position  = elementData.container ? $elem.position() : $elem.offset() ;
-    elementData.container = elementData.container || config.container;
-    elementData.placement = elementData.placement || config.placement;
-    setPositionData(elementData, width, height);
-    elementData.uniqueId = config.uniqueId++;
-    $(elementData.container).append(locastyle.templates.popover(elementData));
-    bindActions($elem, elementData, width, height);
-  }
-
-  function setPositionData(elementData, width, height) {
-    switch (elementData.placement) {
-      case 'top':
-        elementData.position.top -=  12;
-        elementData.position.left +=  (width/2 + 4);
-        break;
-      case 'right':
-        elementData.position.top +=  (height/2 -2);
-        elementData.position.left += (width + 12);
-        break;
-      case 'bottom':
-        elementData.position.top += (height + 12);
-        elementData.position.left +=  (width/2 + 4);
-        break;
-      case 'left':
-        elementData.position.top +=  (height/2 -2 );
-        elementData.position.left -= 12;
+  function buildPopover(index, el) {
+    var elementData = {
+      index : index,
+      title    : $(el).data('title'),
+      content  : $(el).data('content'),
+      placement: $(el).data('placement')
     }
+
+    $(el).after(locastyle.templates.popover(elementData));
   }
 
-  function updateElementPosition($popover, elementData) {
-    $popover.css("top", elementData.position.top).css("left", elementData.position.left);
-  }
 
-  function bindActions($elem, elementData, width, height) {
-    var trigger = elementData.trigger === 'hover' ? config.hoverEvent : config.trigger,
-        $popover = $(config.idPopover + elementData.uniqueId);
-    if(trigger === config.hoverEvent){
-      $elem.on({
-        mouseenter: function(event) {
-          event.preventDefault();
-          elementData.position = $(this).offset() ;
-          setPositionData(elementData, width, height);
-          updateElementPosition($popover, elementData);
-          open($popover);
-        },
-        mouseleave: function (event) {
-          event.preventDefault();
-          close($popover);
-        }
-      });
-    } else {
-      $elem.on({
-        click: function(event) {
-          event.preventDefault();
-          event.stopPropagation();
-          elementData.position = $(this).offset() ;
-          setPositionData(elementData, width, height);
-          updateElementPosition($popover, elementData);
-          if($popover.hasClass("ls-active")) {
-            close($popover);
-          } else {
-            open($popover);
-          }
-        }
-      });
-      $(document).on('click', function(event) {
-        var element = event.toElement;
-        if(!$(element).parents().hasClass( config.popoverClass )){
-          close($('.' + config.popoverClass));
-        }
-      });
-    }
-  }
-
-  function open($popover) {
-    $popover.addClass(config.activeClass).show();
-    $popover.trigger('popover:opened');
-  }
-
-  function close($popover) {
-    $popover.stop().removeClass(config.activeClass).hide();
-    $popover.trigger('popover:closed');
-  }
-
-  function destroy() {
-    $('.' + config.popoverClass).remove();
-    $(config.module).each(function  (index, elem) {
-      $(elem)
-        .removeData('uniqueId')
-        .off(config.hoverEvent)
-        .off(config.trigger);
-    });
-  }
 
   return {
-    init: init,
-    destroyPopover: destroy
+    init: init
   };
 
 }());
