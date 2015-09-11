3f1dba98052a8d60c27bd33119b94265d18902c3
Italo Moraes <italomoraes@outlook.com>
2015-09-11 11:35:37 -0300

    Adding the myTable id to identify the table that will be tested.

--- spec/javascripts/table_spec.js
+++ spec/javascripts/table_spec.js
@@ -7,25 +7,25 @@ describe('Table: ', function() {
 
   describe('when click on checkbox', function() {
     it('should check if the line is checked', function() {
-      var elem = $('tbody tr:first-child');
+      var elem = $('#myTable tbody tr:first-child');
       elem.find('input[type=checkbox]').trigger('click.ls');
       expect(elem.hasClass('is-checked')).toBe(true);
     });
 
     it('should check if all the checkboxes are checked', function() {
-      $('thead tr').find('input[type=checkbox]').trigger('click.ls');
-      $('tbody tr').each(function() {
+      $('#myTable thead tr').find('input[type=checkbox]').trigger('click.ls');
+      $('#myTable tbody tr').each(function() {
         expect($(this).hasClass('is-checked')).toBe(true);
       });
     });
 
     it('should check if all the checkboxes are not checked', function() {
-      var elem = $('thead tr');
+      var elem = $('#myTable thead tr');
       // The first click to select all cheboxes
       elem.find('input[type=checkbox]').trigger('click.ls');
       // The second click to deselect all checkboxes
       elem.find('input[type=checkbox]').trigger('click.ls');
-      $('tbody tr').each(function() {
+      $('#myTable tbody tr').each(function() {
         expect($(this).hasClass('is-checked')).toBe(false);
       });
     });
