f4d69999422ac47e94cfceffd73769dfc3acc71f
Italo Santiago <italo.santiago@locaweb.com.br>
2016-01-28 11:15:33 -0200

    Add clearTimeout inside the changeClass to avoid an infinite loop on mobile window resize

--- source/assets/javascripts/locastyle/_breakpoint-check.js
+++ source/assets/javascripts/locastyle/_breakpoint-check.js
@@ -13,6 +13,9 @@ locastyle.breakpoints = (function() {
     html: null
   };
 
+  //
+  // The userConfig var is used only on Jamine tests
+  //
   function init(userConfig) {
     config.html = $('html');
 
@@ -36,25 +39,25 @@ locastyle.breakpoints = (function() {
     // If less than 768 - xs
     if (documentWidth < config.sm) {
       config.html.addClass('ls-window-xs');
-      locastyle.breakpointClass = "ls-window-xs";
+      locastyle.breakpointClass = 'ls-window-xs';
     }
 
     // If greater than or equal to 768 and less than 992 - sm
     else if (documentWidth >= config.sm && documentWidth < config.md) {
       config.html.addClass('ls-window-sm').removeClass('ls-sidebar-visible ls-notifications-visible ');
-      locastyle.breakpointClass = "ls-window-sm";
+      locastyle.breakpointClass = 'ls-window-sm';
     }
 
     // If greater than or equal to 992 and less than 1200 - md
     else if (documentWidth >= config.md && documentWidth < config.lg) {
       config.html.addClass('ls-window-md').removeClass('ls-sidebar-visible ls-notifications-visible ');
-      locastyle.breakpointClass = "ls-window-md";
+      locastyle.breakpointClass = 'ls-window-md';
     }
 
     // If greater than or equal to 1200 - lg
     else {
       config.html.addClass('ls-window-lg').removeClass('ls-sidebar-visible ls-notifications-visible ');
-      locastyle.breakpointClass = "ls-window-lg";
+      locastyle.breakpointClass = 'ls-window-lg';
     }
   }
 
@@ -63,7 +66,7 @@ locastyle.breakpoints = (function() {
   //
   function breakpointScreenWidth(userConfig) {
     var screenWidth;
-    
+
     if (userConfig){
       screenWidth = userConfig.documentWidth;
     } else {
@@ -73,25 +76,25 @@ locastyle.breakpoints = (function() {
     // If less than 768 - xs
     if (screenWidth < config.sm) {
       config.html.addClass('ls-screen-xs');
-      locastyle.breakpointScreenClass = "ls-screen-xs";
+      locastyle.breakpointScreenClass = 'ls-screen-xs';
     }
 
     // If greater than or equal to 768 and less than 992 - sm
     else if (screenWidth >= config.sm && screenWidth < config.md) {
       config.html.addClass('ls-screen-sm');
-      locastyle.breakpointScreenClass = "ls-screen-sm";
+      locastyle.breakpointScreenClass = 'ls-screen-sm';
     }
 
     // If greater than or equal to 992 and less than 1200 - md
     else if (screenWidth >= config.md && screenWidth < config.lg) {
       config.html.addClass('ls-screen-md');
-      locastyle.breakpointScreenClass = "ls-screen-md";
+      locastyle.breakpointScreenClass = 'ls-screen-md';
     }
 
     // If greater than or equal to 1200 - lg
     else {
       config.html.addClass('ls-screen-lg');
-      locastyle.breakpointScreenClass = "ls-screen-lg";
+      locastyle.breakpointScreenClass = 'ls-screen-lg';
     }
   }
 
@@ -106,7 +109,6 @@ locastyle.breakpoints = (function() {
       clearTimeout(changeClass);
 
       changeClass = setTimeout(function() {
-
         var breakpointActive = config.html.attr('class').replace(/(^|\s)(ls-window-\S+)|(ls-screen-\S+)/g, '');
 
         config.html.attr('class', $.trim(breakpointActive));
@@ -115,7 +117,9 @@ locastyle.breakpoints = (function() {
         breakpointScreenWidth();
 
         // event triggers to inform other modules that the breakpoint has been updated
-        $.event.trigger("breakpoint-updated");
+        $.event.trigger('breakpoint-updated');
+
+        clearTimeout(changeClass);
       }, 300);
 
     });
