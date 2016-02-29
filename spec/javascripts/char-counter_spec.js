bea1eb58a2ff769a09dfe3c4c6f66987cd2c4958
Italo Santiago <itu.moraes@gmail.com>
2016-02-29 15:04:21 -0300

    Char counter prefix tests

--- spec/javascripts/char-counter_spec.js
+++ spec/javascripts/char-counter_spec.js
@@ -16,7 +16,14 @@ describe("Counter char: ", function(){
       $inputMaxlength.trigger(event);
       expect($("#myCounterTrigger").find('.ls-char-count').text()).toEqual('6');
     });
-
   });
 
+  describe('When the input node is a .ls-prefix-group children', function () {
+    it('should create a element with .ls-char-count after the ' +
+      '.ls-prefix-group element', function() {
+      expect($('#prefixCounter').find('.ls-prefix-group').next()
+        .find('.ls-char-count')).toEqual($('#prefixCounter')
+        .find('.ls-char-count'));
+    });
+  });
 });
