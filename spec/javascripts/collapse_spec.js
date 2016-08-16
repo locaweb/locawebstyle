76b78ed319f343b036ba577f12b44357bd16c38e
Italo Santiago <itu.moraes@gmail.com>
2016-08-16 18:35:38 -0300

    Add collapse aria and element trigger specs

--- spec/javascripts/collapse_spec.js
+++ spec/javascripts/collapse_spec.js
@@ -1,9 +1,7 @@
 describe('Collapse:', function() {
-
   beforeEach(function() {
     loadFixtures('collapse_fixture.html');
     locastyle.collapse.init();
-
   });
 
   describe('When click to open collapse', function() {
@@ -12,16 +10,57 @@ describe('Collapse:', function() {
       expect($('#collapse1')).toBeVisible();
     });
 
-    it("should trigger the event collapse:opened", function() {
+    it('should trigger the event collapse:opened', function() {
       var spyEvent = spyOnEvent(document, 'collapse:opened');
-      $('#myCollapse1 .ls-collapse-header').trigger("click");
+      $('#myCollapse1 .ls-collapse-header').trigger('click');
       expect('collapse:opened').toHaveBeenTriggeredOn(document);
 
       var spyEvent = spyOnEvent(document, 'collapse:closed');
-      $('#myCollapse1 .ls-collapse-header').trigger("click");
+      $('#myCollapse1 .ls-collapse-header').trigger('click');
       expect('collapse:closed').toHaveBeenTriggeredOn(document);
     });
+  });
 
+  describe('when the collapse is opened', function() {
+    var collapse = null;
+    var collapseHeader = null;
+    var collapseBody = null;
+
+    beforeEach(function() {
+      collapse = $('#myCollapse1');
+      collapseHeader = collapse.find('.ls-collapse-header');
+      collapseBody = collapse.find('.ls-collapse-body');
+      collapseHeader.trigger('click');
+    });
+
+    it('should add the aria-expanded true to collapse header', function() {
+      expect(collapseHeader.attr('aria-expanded')).toBe('true');
+    });
+
+    it('should add the aria-hidden false to collapse header', function() {
+      expect(collapseBody.attr('aria-hidden')).toBe('false');
+    });
+
+    describe('when the collapse is close', function() {
+      beforeEach(function() {
+        collapseHeader.trigger('click');
+      });
+
+      it('should add the aria-expanded false to collapse header', function() {
+        expect(collapseHeader.attr('aria-expanded')).toBe('false');
+      });
+
+      it('should add the aria-hidden true to collapse header', function() {
+        expect(collapseBody.attr('aria-hidden')).toBe('true');
+      });
+    });
+  });
+
+  describe('when use another element to trigger the collapse', function() {
+    it('should toggle the collapse state', function() {
+      $('#button-trigger').trigger('click');
+      expect($('#myCollapse4')).toBeVisible();
+    });
   });
 
   describe('When collapse have a class ls-collapse-opened', function() {
@@ -37,22 +76,19 @@ describe('Collapse:', function() {
     });
   });
 
-
   describe('Group / Accordeon', function() {
-
     it('open collapse, close others', function() {
       var $collapseClose = $('#collapse4');
       var $collapseCloseBody = $collapseClose.find('.ls-collapse-body');
       var $collapseOpen = $('#collapse5');
       var $collapseOpenTitle = $collapseOpen.find('.ls-collapse-body');
-      $collapseOpen.trigger("click");
+
+      $collapseOpen.trigger('click');
       expect($collapseOpen.is(':visible')).toBe(true);
       expect($collapseClose.is(':visible')).toBe(true);
-      $collapseClose.trigger("click");
+      $collapseClose.trigger('click');
       expect($collapseOpen.is(':visible')).toBe(true);
       expect($collapseClose.is(':visible')).toBe(true);
     });
-
   });
-
 });
